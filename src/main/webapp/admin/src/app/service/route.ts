ac7051e6a2ee760419b57dfec5d55b0dd5f7d37a
aaron <462826@qq.com>
2017-01-17 15:29:07 +0800

    tt

---
+++ src/main/webapp/admin/src/app/service/route.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Injectable()
+export class RouteService {
+    constructor(private _router: Router) { }
+
+    navTo(url: string) {
+      let that = this;
+      let urlStr = that._router.createUrlTree([url, {ignore_menu: true}]);
+      that._router.navigateByUrl(urlStr);
+    }
+}
+

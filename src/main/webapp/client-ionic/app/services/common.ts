37f93ab3fc468840baf30cf5e162bba5ade31129
aaron <462826@qq.com>
2017-02-21 18:40:50 +0800

    tt

--- src/main/webapp/client-ionic/app/services/common.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from '@angular/core';
-
-import {CONSTANT} from '../utils/constant';
-
-@Injectable()
-export class CommonService {
-    constructor() { }
-
-}
--- src/main/webapp/client-wechat/services/about.js
+++
@@ -1,9 +0,0 @@
-var constant = require('../utils/constant.js')
-var post = require('./post.js')
-
-function doSomething(cb){
-   
-}
-module.exports = {
-  doSomething: doSomething
-}

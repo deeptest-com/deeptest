fab6a27424ffb5e4679f0e25d6e413dbc866f673
aaron.chen <aaron.chen@aispeech.com>
2017-06-27 21:26:53 +0800

    开发用例设计功能

--- src/main/webapp/client/src/app/components/custom-field/index.ts
+++ src/main/webapp/client/src/app/components/custom-field/index.ts
@@ -4,22 +4,16 @@ import { FieldEvent, FieldChangedEvent } from './src/field.events';
 import { CustomFieldComponent } from './src/custom-field.component';
 import { CustomFieldService } from './src/custom-field.service';
 
-import { InputTextComponent } from './src/input-text/input-text.component';
-import { InputTextService } from './src/input-text/input-text.service';
-
 import {CustomFieldModule} from "./src/custom-field.module";
 
 export {
-  FieldType, Prop, Field, InputField,
+  FieldType, Prop, Field,
 
   FieldEvent,
   FieldChangedEvent,
 
   CustomFieldComponent,
   CustomFieldService,
-
-  InputTextComponent,
-  InputTextService,
 
   CustomFieldModule
 };
--- src/main/webapp/client/src/app/components/custom-field/src/custom-field.module.ts
+++ src/main/webapp/client/src/app/components/custom-field/src/custom-field.module.ts
@@ -5,14 +5,11 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { CustomFieldComponent } from './custom-field.component';
 import { CustomFieldService } from './custom-field.service';
 
-import { InputTextComponent } from './input-text/input-text.component';
-import { InputTextService } from './input-text/input-text.service';
-
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  declarations: [CustomFieldComponent, InputTextComponent],
-  exports: [CustomFieldComponent, InputTextComponent],
-  providers: [CustomFieldService, InputTextService]
+  declarations: [CustomFieldComponent],
+  exports: [CustomFieldComponent],
+  providers: [CustomFieldService]
 })
 export class CustomFieldModule {
 

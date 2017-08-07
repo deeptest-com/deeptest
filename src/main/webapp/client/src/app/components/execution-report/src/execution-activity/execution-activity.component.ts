f86ce81b65fdb901f90427899681d165d7a05ed4
aaron.chen <aaron.chen@aispeech.com>
2017-08-07 15:48:21 +0800

    测试计划执行图表

---
+++ src/main/webapp/client/src/app/components/execution-report/src/execution-activity/execution-activity.component.ts
@@ -0,0 +1,83 @@
+import {Component, Input, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'execution-activity',
+  templateUrl: './execution-activity.html',
+  styleUrls: ['./styles.scss']
+})
+export class ExecutionActivityComponent implements OnInit {
+
+  @Input() data: any;
+  chartOption: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): any {
+    this.chartOption = {
+      title: {
+        show: false
+      },
+      tooltip : {
+        trigger: 'axis',
+        axisPointer : {
+          type : 'shadow'
+        }
+      },
+      grid: {
+        top: '5%',
+        right: '15%',
+        bottom: '0%',
+        left: '1%',
+        containLabel: true
+      },
+      legend: {
+        right: '0%',
+        width: '15%',
+        data:['通过','失败','阻塞','未执行']
+      },
+      color: ['#749f83', '#c23531', '#ca8622', '#c4ccd3'],
+
+      xAxis : [
+        {
+          type : 'category',
+          data : ['07-01','07-02','07-03','07-04','07-05','07-06','07-07']
+        }
+      ],
+      yAxis : [
+        {
+          type : 'value'
+        }
+      ],
+      series : [
+
+        {
+          name:'通过',
+          type:'bar',
+          stack: '广告',
+          data:[120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name:'失败',
+          type:'bar',
+          stack: '广告',
+          data:[220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name:'阻塞',
+          type:'bar',
+          stack: '广告',
+          data:[150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name:'未执行',
+          type:'bar',
+          stack: '广告',
+          data:[150, 232, 201, 154, 190, 40, 20]
+        }
+
+      ]
+    };
+  }
+
+}

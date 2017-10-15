58504ead029ed4b4f65697387fa1b5119a149af2
aaron.chen <aaron.chen@aispeech.com>
2017-10-15 15:25:52 +0800

    测试执行

--- src/main/webapp/client/src/app/pages/admin-sys/sys-admin.component.ts
+++ src/main/webapp/client/src/app/pages/admin-sys/sys-admin.component.ts
@@ -8,10 +8,10 @@ import {Component} from "@angular/core";
 export class SysAdmin {
 
   public menuItems:any[] = [
-    {link:'/pages/sys-admin/user/list', title: '用户'},
-    {link:'/pages/sys-admin/group/list', title: '群组'},
-    {link:'/pages/sys-admin/role/list', title: '角色'},
-    {link:'/pages/sys-admin/custom', title: '属性定义'},
+    {link:'/pages/sys-admin/user/list', title: '系统用户'},
+    {link:'/pages/sys-admin/group/list', title: '系统群组'},
+    {link:'/pages/sys-admin/role/list', title: '系统角色'},
+    /*{link:'/pages/sys-admin/custom', title: '属性定义'},*/
     {link:'/pages/sys-admin/settings', title: '系统配置'},
     {link:'/pages/sys-admin/integration', title: '第三方集成'}
   ];

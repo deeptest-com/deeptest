67cde520d877ebfa6fe3681331718c9f3f23f07d
aaron.chen <aaron.chen@aispeech.com>
2018-06-26 19:28:18 +0800

    init

--- src/main/java/com/ngtesting/platform/WebSocketConfig.java
+++ src/main/java/com/ngtesting/platform/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.setApplicationDestinationPrefixes("/ws");
     }
 
     @Override
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -33,7 +33,7 @@ function disconnect() {
 }
 
 function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+    stompClient.send("/ws/hello", {}, JSON.stringify({'name': $("#name").val()}));
 }
 
 function showGreeting(message) {

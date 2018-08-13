d3772025b4cfb1ef11a6dc94bb5f91e2f6c3f819
aaron.chen <aaron.chen@aispeech.com>
2018-08-13 19:21:02 +0800

    增加客户端文件

---
+++ src/main/java/com/ngtesting/platform/dao/WelcomeDao.java
@@ -0,0 +1,9 @@
+package com.ngtesting.platform.dao;
+
+import com.ngtesting.platform.model.SysNums;
+
+import java.util.List;
+
+public interface WelcomeDao {
+    List<SysNums> test();
+}
--- src/main/java/com/ngtesting/platform/service/impl/WelcomeServiceImpl.java
+++ src/main/java/com/ngtesting/platform/service/impl/WelcomeServiceImpl.java
@@ -1,23 +1,23 @@
 package com.ngtesting.platform.service.impl;
 
+import com.ngtesting.platform.dao.WelcomeDao;
 import com.ngtesting.platform.model.SysNums;
 import com.ngtesting.platform.service.WelcomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class WelcomeServiceImpl extends BaseServiceImpl implements WelcomeService {
+    @Autowired
+    WelcomeDao welcomeDao;
 
     @Override
     public List<SysNums> test() {
-//        DetachedCriteria dc = DetachedCriteria.forClass(SysNums.class);
-//        dc.add(Restrictions.eq("key", Long.valueOf(1)));
-//        List<SysNums> pos = findAllByCriteria(dc);
-//
-//        return pos;
+        List<SysNums> pos = welcomeDao.test();
 
-        return null;
+        return pos;
     }
 
 }

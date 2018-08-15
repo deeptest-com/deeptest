58be9d8e5ff113c395a6338ad333e230d266e77f
aaron.chen <aaron.chen@aispeech.com>
2018-08-15 20:55:24 +0800

    增加数据的权限控制

---
+++ src/test/java/com/ngtesting/platform/Test.java
@@ -0,0 +1,10 @@
+package com.ngtesting.platform;
+
+
+import com.ngtesting.platform.utils.FileUtil;
+
+public class Test {
+    public static void main(String args[]) {
+        FileUtil.CreateDirIfNeeded("/work/ngtesting-data/");
+    }
+}

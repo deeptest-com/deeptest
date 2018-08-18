c51531ff203b1373ccac6a0c5260308d1f68a351
aaron.chen <aaron.chen@aispeech.com>
2018-08-18 13:44:45 +0800

    修复SpringBoot打包文件内置邮件模板的问题

--- src/main/java/com/ngtesting/platform/service/MailService.java
+++ src/main/java/com/ngtesting/platform/service/MailService.java
@@ -7,10 +7,4 @@ public interface MailService extends BaseService {
 	void send(String subject, String text, String toEmail);
 	void sendTemplateMail(String subject, String templateName, String toEmail, Map<String, String> map);
 
-	String getClassesPath(Class<?> cls);
-
-	String getAppPath(Class<?> cls);
-	String getFileName(String path);
-	String getFilePath();
-
 }

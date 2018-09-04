19e1898d23419970761ebf317927aa07d7f97423
aaron.chen <aaron.chen@aispeech.com>
2018-09-04 12:40:43 +0800

    自动生成带版本的Docker镜像

--- src/main/docker/mysql/setup.sh
+++ src/main/docker/mysql/setup.sh
@@ -8,20 +8,26 @@ service mysql start
 sleep 3
 echo `service mysql status`
 
-echo '2. 开始导入数据....'
-mysql < /etc/mysql/database.sql
-mysql ngtesting-web < /etc/mysql/schema.sql
-echo '3. 成功导入数据....'
+if [ -f ~/init ] ; then
+    echo "数据已初始化，退出"
+else
+    echo '2. 开始导入数据....'
+    mysql < /etc/mysql/database.sql
+    mysql ngtesting-web < /etc/mysql/schema.sql
+    echo '3. 成功导入数据....'
 
-sleep 3
-echo `service mysql status`
+    sleep 3
+    echo `service mysql status`
 
-echo '4. 开始修改密码....'
-mysql < /etc/mysql/privileges.sql
-echo '5. 修改密码完毕....'
+    echo '4. 开始修改密码....'
+    mysql < /etc/mysql/privileges.sql
+    echo '5. 修改密码完毕....'
 
-#sleep 3
-echo `service mysql status`
-echo 'mysql容器启动完毕'
+    #sleep 3
+    echo `service mysql status`
+    echo 'mysql容器启动完毕'
+
+    touch ~/init
+fi
 
 tail -f /dev/null

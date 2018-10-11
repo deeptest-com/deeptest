dd9309e6b166cb602eb8f6174aab6ee225c4baa6
Aaron Chen <462826@qq.com>
2018-10-12 07:17:28 +0800

    使用AOP鉴权后台管理接口

--- src/main/java/com/ngtesting/platform/config/AuthAminAspect.java
+++ src/main/java/com/ngtesting/platform/config/AuthAminAspect.java
@@ -1,9 +1,7 @@
 package com.ngtesting.platform.config;
 
-import com.alibaba.fastjson.JSONObject;
 import com.ngtesting.platform.dao.AuthDao;
 import com.ngtesting.platform.exception.AuthException;
-import com.ngtesting.platform.model.TstOrg;
 import com.ngtesting.platform.model.TstUser;
 import com.ngtesting.platform.service.AuthService;
 import org.aspectj.lang.JoinPoint;
@@ -11,17 +9,14 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.annotation.Annotation;
 
 @Aspect
 @Component
@@ -38,28 +33,19 @@ public class AuthAminAspect {
 
     @Before("authAmin()")
     public void doBefore(JoinPoint joinPoint) throws Throwable {
-        Boolean hasOrgAdminPriviledge = false;
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        if (className.equals("OrgAdmin")) {
+            return;
+        }
 
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
         TstUser user = (TstUser) request.getSession().getAttribute(Constant.HTTP_SESSION_USER_PROFILE);
 
         Integer orgId = getOrgId(joinPoint);
-        if (orgId != null) {
-            hasOrgAdminPriviledge = authService.hasOrgAdminPrivilege(user.getId(), orgId);
-        } else {
-            hasOrgAdminPriviledge = true;
-        }
-
-        if (!hasOrgAdminPriviledge) {
+        if (orgId != null && !authService.hasOrgAdminPrivilege(user.getId(), orgId)) {
             throw new AuthException();
         }
-
-//        logger.info("URL : " + request.getRequestURL().toString());
-//        logger.info("HTTP_METHOD : " + request.getMethod());
-//        logger.info("IP : " + request.getRemoteAddr());
-//        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-//        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
     }
 
     @AfterReturning(returning = "ret", pointcut = "authAmin()")
@@ -68,43 +54,34 @@ public class AuthAminAspect {
     }
 
     Integer getOrgId(JoinPoint joinPoint) throws Throwable {
-        Integer orgId = null;
-
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
 
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        Annotation[][] annotationMatrix = methodSignature.getMethod().getParameterAnnotations();
-        int index = -1;
-        for (Annotation[] annotations : annotationMatrix) {
-            index++;
-            for (Annotation annotation : annotations) {
-                if (annotation instanceof RequestBody) {
-                    Object requestBody = joinPoint.getArgs()[index];
-                    if (requestBody instanceof JSONObject) {
-                        logger.info("RequestBody: " + requestBody);
+//        logger.info("URL : " + request.getRequestURL().toString());
+//        logger.info("HTTP_METHOD : " + request.getMethod());
+//        logger.info("IP : " + request.getRemoteAddr());
+//        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+//        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
 
-                        JSONObject json = (JSONObject) requestBody;
-                        if (json.getInteger("orgId") != null) {
-                            orgId = json.getInteger("orgId");
-                        }
-                    } else if (requestBody instanceof TstOrg) {
-                        TstOrg json = (TstOrg) requestBody;
-                        orgId = json.getId();
-                    }
-                }
-            }
-        }
+        TstUser user = (TstUser) request.getSession().getAttribute(Constant.HTTP_SESSION_USER_PROFILE);
 
-        if (orgId == null) {
-            TstUser user = (TstUser) request.getSession().getAttribute(Constant.HTTP_SESSION_USER_PROFILE);
-            logger.info("TstUser: " + user);
-
-            orgId = user.getDefaultOrgId();
-        }
-
-        logger.info("***orgId: " + orgId);
+        Integer orgId = user.getDefaultOrgId();
         return orgId;
     }
+
+//        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+//        Annotation[][] annotationMatrix = methodSignature.getMethod().getParameterAnnotations();
+//        int index = -1;
+//        for (Annotation[] annotations : annotationMatrix) {
+//            index++;
+//            for (Annotation annotation : annotations) {
+//                if (annotation instanceof RequestBody) {
+//                    Object requestBody = joinPoint.getArgs()[index];
+//                    if (requestBody instanceof JSONObject) {
+//                        logger.info("RequestBody: " + requestBody);
+//                    }
+//                }
+//            }
+//        }
 
 }

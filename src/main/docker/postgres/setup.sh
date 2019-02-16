e515f2ee618827ff3855abb7c85a7ecade1e983e
Aaron Chen <462826@qq.com>
2019-02-16 09:53:29 +0800

    数据库迁移到postgresql

--- src/main/docker/postgres/setup.sh
+++ src/main/docker/postgres/setup.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-echo '1. 检测postgres状态'
-echo "localhost:*:*:ngtesting:P2ssw0rd" > $HOME/.pgpass
-echo "`chmod 0600 $HOME/.pgpass`"
-count=`psql -h localhost -U ngtesting ngtesting-web  -c "select tablename from pg_tables where schemaname='public'" | grep TstProject`
+echo '1. 安装postgres客户端'
+apt-get install postgresql-client
+echo '1. 成功安装postgres客户端'
+
+echo '2. 检测postgres状态'
+export PGPASSWORD=P2ssw0rd;
+count=`psql -h ngtesting-postgres -U ngtesting ngtesting-web  -c "select tablename from pg_tables where schemaname='public'" | grep TstProject`
 echo "count=$count"
 
-set -e  # Exit the script if an error happens
+#set -e
 
 if [ "$count" != "" ]; then
     echo "数据已初始化，退出"
 else
-    echo '2. 创建用户....'
-    su - postgres -c "psql  -c \"CREATE USER ngtesting WITH PASSWORD 'P2ssw0rd'\""
-    echo '2. 成功创建用户'
+    echo '3. 创建用户....'
+    psql -h ngtesting-postgres -U postgres -c "CREATE USER ngtesting WITH PASSWORD 'P2ssw0rd'
+    echo '3. 成功创建用户'
 
-    echo '3. 创建数据库....'
-    su - postgres -c "psql  -c 'CREATE DATABASE \"ngtesting-web\" OWNER ngtesting'"
-    echo '3. 成功创建数据库'
+    echo '4. 创建数据库....'
+    psql -h ngtesting-postgres -U postgres  -c 'CREATE DATABASE "ngtesting-web" OWNER ngtesting'
+    echo '4. 成功创建数据库'
 
-    echo '4. 导入数据....'
-    su - postgres -c "psql ngtesting-web -U ngtesting < /schema.sql"
-    echo '4. 成功导入数据'
+    echo '5. 导入数据....'
+    psql -h ngtesting-postgres -U ngtesting "ngtesting-web" < /schema.sql
+    echo '5. 成功导入数据'
 
     touch ~/init-success
 fi
+
+tail -f /dev/null

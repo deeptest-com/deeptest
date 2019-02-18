7f64a67097731d6e83fdcb6d58b09238d029be58
Aaron Chen <462826@qq.com>
2019-02-19 00:33:16 +0800

    缺陷全文检索

--- src/main/docker/postgres/setup.sh
+++ src/main/docker/postgres/setup.sh
@@ -18,13 +18,15 @@ else
     psql -h ngtesting-postgres -U postgres  -c 'CREATE DATABASE "ngtesting-web" OWNER ngtesting'
     echo '3. 成功创建数据库'
 
-    psql -h ngtesting-postgres -U ngtesting ngtesting-web  -c 'CREATE EXTENSION zhparser;'
-    psql -h ngtesting-postgres -U ngtesting ngtesting-web  -c 'CREATE TEXT SEARCH CONFIGURATION chinese_zh (PARSER = zhparser);'
-    psql -h ngtesting-postgres -U ngtesting ngtesting-web  -c 'ALTER TEXT SEARCH CONFIGURATION chinese_zh ADD MAPPING FOR n,v,a,i,e,l WITH simple;'
-
     echo '4. 导入数据....'
     psql -h ngtesting-postgres -U ngtesting "ngtesting-web" < /schema.sql
     echo '4. 成功导入数据'
+
+    echo '5. 初始化全文检索....'
+    psql -h ngtesting-postgres -U ngtesting ngtesting-web  -c 'CREATE EXTENSION zhparser;'
+    psql -h ngtesting-postgres -U ngtesting ngtesting-web  -c 'CREATE TEXT SEARCH CONFIGURATION chinese_zh (PARSER = zhparser);'
+    psql -h ngtesting-postgres -U ngtesting ngtesting-web  -c 'ALTER TEXT SEARCH CONFIGURATION chinese_zh ADD MAPPING FOR n,v,a,i,e,l WITH simple;'
+    echo '4. 成功初始化全文检索'
 
     touch ~/init-success
 fi

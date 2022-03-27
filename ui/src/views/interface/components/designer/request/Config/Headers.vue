add98c3e3ef496e978471b70f9132df40381e34c
aaron <462826@qq.com>
2022-03-27 11:40:06 +0800

    build request data

--- ui/src/views/interface/components/designer/request/Config/Headers.vue
+++ ui/src/views/interface/components/designer/request/Config/Headers.vue
@@ -9,40 +9,45 @@
             <QuestionCircleOutlined class="dp-icon-btn dp-trans-80"/>
           </a-tooltip>
 
-          <a-tooltip overlayClassName="dp-tip-small">
+          <a-tooltip @click="removeAll" overlayClassName="dp-tip-small">
             <template #title>全部清除</template>
             <DeleteOutlined class="dp-icon-btn dp-trans-80"/>
           </a-tooltip>
 
-          <a-tooltip overlayClassName="dp-tip-small">
+          <a-tooltip @click="add" overlayClassName="dp-tip-small">
             <template #title>新增</template>
             <PlusOutlined class="dp-icon-btn dp-trans-80"/>
           </a-tooltip>
         </a-col>
       </a-row>
     </div>
-    <div class="params">
-      <a-row v-for="(item, idx) in requestData.children" :key="idx" type="flex" class="param">
+    <div class="headers">
+      <a-row v-for="(item, idx) in requestData.headers" :key="idx" type="flex" class="header">
         <a-col flex="1">
-          <a-input v-model:value="item.key" @change="onParamChange(idx)" class="dp-bg-input-transparent" />
+          <a-input v-model:value="item.name" @change="onParamChange(idx)" class="dp-bg-input-transparent" />
         </a-col>
         <a-col flex="1">
-          <a-input v-model:value="item.val" @change="onParamChange(idx)" class="dp-bg-input-transparent" />
+          <a-input v-model:value="item.value" @change="onParamChange(idx)" class="dp-bg-input-transparent" />
         </a-col>
         <a-col flex="80px" class="dp-right dp-color-primary dp-icon-btn-container">
-          <a-tooltip overlayClassName="dp-tip-small">
+          <a-tooltip v-if="!item.disabled" @click="disable(idx)" overlayClassName="dp-tip-small">
             <template #title>禁用</template>
             <CheckCircleOutlined class="dp-icon-btn dp-trans-80" />
           </a-tooltip>
 
-          <a-tooltip overlayClassName="dp-tip-small">
+          <a-tooltip v-if="item.disabled" @click="disable(idx)" overlayClassName="dp-tip-small">
             <template #title>启用</template>
             <CloseCircleOutlined class="dp-icon-btn dp-trans-80" />
           </a-tooltip>
 
-          <a-tooltip overlayClassName="dp-tip-small">
+          <a-tooltip @click="remove(idx)" overlayClassName="dp-tip-small">
             <template #title>移除</template>
             <DeleteOutlined class="dp-icon-btn dp-trans-80"/>
+          </a-tooltip>
+
+          <a-tooltip @click="insert(idx)" overlayClassName="dp-tip-small">
+            <template #title>插入</template>
+            <PlusOutlined class="dp-icon-btn dp-trans-80"/>
           </a-tooltip>
         </a-col>
       </a-row>
@@ -56,40 +61,57 @@ import {useI18n} from "vue-i18n";
 import {useStore} from "vuex";
 import { QuestionCircleOutlined, DeleteOutlined, PlusOutlined, CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons-vue';
 import {StateType} from "@/views/interface/store";
-
-interface RequestHeadersSetupData {
-  requestData: ComputedRef;
-
-  onParamChange: (idx) => void;
-  doSomething: (e) => void;
-}
+import {Header, Request} from "@/views/interface/data";
 
 export default defineComponent({
   name: 'RequestHeaders',
   components: {
     QuestionCircleOutlined, DeleteOutlined, PlusOutlined, CheckCircleOutlined, CloseCircleOutlined,
   },
-  setup(props): RequestHeadersSetupData {
+  setup(props) {
     const {t} = useI18n();
     const store = useStore<{ Interface: StateType }>();
-    const requestData = computed<any>(() => store.state.Interface.requestData);
+    const requestData = computed<Request>(() => store.state.Interface.requestData);
 
     const onParamChange = (idx) => {
       console.log('onParamChange', idx)
-      if (requestData.value.children.length <= idx + 1
-          && (requestData.value.children[idx].key !== '' || requestData.value.children[idx].val !== '')) {
-        requestData.value.children.push({})
+      if (requestData.value.headers.length <= idx + 1
+          && (requestData.value.headers[idx].name !== '' || requestData.value.headers[idx].value !== '')) {
+        requestData.value.headers.push({} as Header)
       }
     };
 
-    const doSomething = (e) => {
-      console.log('doSomething', e)
-    };
+    const add = () => {
+      console.log('add')
+      requestData.value.params.push({} as Header)
+    }
+    const removeAll = () => {
+      console.log('removeAll', requestData.value.headers)
+      requestData.value.headers = [{} as Header]
+    }
+
+    const disable = (idx) => {
+      console.log('enable', idx)
+      requestData.value.headers[idx].disabled = !requestData.value.headers[idx].disabled
+    }
+    const remove = (idx) => {
+      console.log('remove')
+      requestData.value.headers.splice(idx, 1)
+      add()
+    }
+    const insert = (idx) => {
+      console.log('insert')
+      requestData.value.headers.splice(idx+1, 0, {} as Header)
+    }
 
     return {
       requestData,
       onParamChange,
-      doSomething,
+      add,
+      removeAll,
+      disable,
+      remove,
+      insert,
     }
   }
 })
@@ -103,10 +125,10 @@ export default defineComponent({
     padding: 2px 3px;
     border-bottom: 1px solid #d9d9d9;
   }
-  .params {
+  .headers {
     height: calc(100% - 28px);
     overflow-y: auto;
-    .param {
+    .header {
       padding: 2px 3px;
       border-bottom: 1px solid #d9d9d9;
 

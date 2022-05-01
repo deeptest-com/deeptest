ac1ca0ceb469fdd5b7f4a138783656734b896f56
aaron <462826@qq.com>
2022-05-01 16:29:13 +0800

    response extractors and checkpoints

--- internal/server/modules/v1/service/extractor.go
+++ internal/server/modules/v1/service/extractor.go
@@ -1,8 +1,13 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
+	serverConsts "github.com/aaronchen2k/deeptest/internal/server/consts"
+	serverDomain "github.com/aaronchen2k/deeptest/internal/server/modules/v1/domain"
 	"github.com/aaronchen2k/deeptest/internal/server/modules/v1/model"
 	"github.com/aaronchen2k/deeptest/internal/server/modules/v1/repo"
+	"github.com/oliveagle/jsonpath"
 )
 
 type ExtractorService struct {
@@ -11,7 +16,7 @@ type ExtractorService struct {
 }
 
 func (s *ExtractorService) List(interfaceId int) (extractors []model.InterfaceExtractor, err error) {
-	extractors, err = s.ExtractorRepo.List(interfaceId)
+	extractors, err = s.ExtractorRepo.List(uint(interfaceId))
 
 	return
 }
@@ -36,6 +41,46 @@ func (s *ExtractorService) Update(extractor *model.InterfaceExtractor) (err erro
 
 func (s *ExtractorService) Delete(reqId uint) (err error) {
 	err = s.ExtractorRepo.Delete(reqId)
+
+	return
+}
+
+func (s *ExtractorService) ExtractByInterface(interfaceId uint, resp serverDomain.InvocationResponse) (err error) {
+	extractors, _ := s.ExtractorRepo.List(interfaceId)
+
+	for _, extractor := range extractors {
+		s.Extract(extractor, resp)
+	}
+
+	return
+}
+
+func (s *ExtractorService) Extract(extractor model.InterfaceExtractor, resp serverDomain.InvocationResponse) (err error) {
+	if extractor.Src == serverConsts.Header {
+		for _, h := range resp.Headers {
+			if h.Name == extractor.Expression {
+				extractor.Result = h.Value
+				break
+			}
+		}
+
+		s.ExtractorRepo.UpdateResult(extractor)
+		return
+	}
+
+	var jsonData interface{}
+	json.Unmarshal([]byte(resp.Content), &jsonData)
+
+	if extractor.Type == serverConsts.JsonPath {
+		pat, _ := jsonpath.Compile(extractor.Expression)
+		result, err := pat.Lookup(jsonData)
+
+		if err == nil && result != nil {
+			extractor.Result = fmt.Sprintf("%v", result)
+		}
+	}
+
+	s.ExtractorRepo.UpdateResult(extractor)
 
 	return
 }

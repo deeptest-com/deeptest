248dd58da4089ec1dc21c9705e9eacdc871022c2
aaron <462826@qq.com>
2022-12-16 19:28:54 +0800

    right click response to extract variable

--- ui/src/components/Editor/service.ts
+++ ui/src/components/Editor/service.ts
@@ -5,7 +5,7 @@ export function addExtractAction(editor) {
 
     editor.addAction({
         id: 'right-click-text',
-        label: '提取',
+        label: '提取为变量',
 
         keybindings: [
             monaco.KeyMod.CtrlCmd | monaco.KeyCode.F10,
@@ -36,4 +36,32 @@ export function addExtractAction(editor) {
 export function addReplaceAction(editor) {
     console.log('addReplaceAction')
 
+    editor.addAction({
+        id: 'right-click-text',
+        label: '替换为变量',
+
+        keybindings: [
+            monaco.KeyMod.CtrlCmd | monaco.KeyCode.F10,
+            // chord
+            monaco.KeyMod.chord(
+                monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyK,
+                monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyM
+            )
+        ],
+
+        // A precondition for this action.
+        precondition: null,
+
+        // A rule to evaluate on top of the precondition in order to dispatch the keybindings.
+        keybindingContext: null,
+
+        contextMenuGroupId: 'navigation',
+
+        contextMenuOrder: 1.5,
+
+        run: function (ed) {
+            // console.log(ed.getValue());
+            console.log(ed.getModel().getValueInRange(ed.getSelection()), ed.getSelection());
+        }
+    });
 }

8b20bc05cd599c89d1afd567c813818251d431c3
yangxi <yangxi@nancal.com>
2023-01-03 20:47:59 +0800

    modify views

--- ui/src/views/reportNew/detail/index.vue
+++ ui/src/views/reportNew/detail/index.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="scenario-detail-main">
-    <a-table :columns="columns" :data-source="data">
-      <template #name="{ text }">
-        <a>{{ text }}</a>
-      </template>
-      <template #customTitle>
+  <div class="report-main">
+    <a-card :bordered="false">
+      <div>
+        <a-page-header
+            class="demo-page-header"
+            style="border: 1px solid rgb(235, 237, 240)"
+            title="测试报告-{{ modelRef.name }}"
+            @back="() => $router.go(-1)"
+        >
+          <a-descriptions size="small" :column="1">
+            <a-descriptions-item label="Created" >Lili Qu</a-descriptions-item>
+            <a-descriptions-item label="Association">
+              <a>421421</a>
+            </a-descriptions-item>
+            <a-descriptions-item label="Creation Time">2017-01-10</a-descriptions-item>
+            <a-descriptions-item label="Effective Time">2017-10-10</a-descriptions-item>
+            <a-descriptions-item label="Remarks">
+              Gonghu Road, Xihu District, Hangzhou, Zhejiang, China
+            </a-descriptions-item>
+          </a-descriptions>
+        </a-page-header>
+
+            <a-button key="2" type="primary" display="display">全部收起</a-button>
+            <a-button key="1" type="primary" display="true">全部展开</a-button>
+
+      </div>
+      <div class="scenario-detail-main">
+        <a-table :columns="columns" :data-source="data">
+          <template #name="{ text }">
+            <a>{{ text }}</a>
+          </template>
+          <template #customTitle>
       <span>
         <smile-outlined />
         Name
       </span>
-      </template>
-      <template #tags="{ text: tags }">
+          </template>
+          <template #tags="{ text: tags }">
       <span>
         <a-tag
             v-for="tag in tags"
@@ -20,37 +46,25 @@
           {{ tag.toUpperCase() }}
         </a-tag>
       </span>
-      </template>
-      <template #action="{ record }">
-      <span>
-        <a>Invite 一 {{ record.name }}</a>
-        <a-divider type="vertical" />
-        <a>Delete</a>
-        <a-divider type="vertical" />
-        <a class="ant-dropdown-link">
-          More actions
-          <down-outlined />
-        </a>
-      </span>
-      </template>
-    </a-table>
-  </div>
+          </template>
+        </a-table>
+      </div>
 
-  <div class="scenario-detail-main">
-    <a-card :bordered="false" :bodyStyle="{paddingTop: '8px'}">
-      <template #title>
-        <div>报告详情 - {{modelRef.name}}</div>
-      </template>
-      <template #extra>
-        <a-button type="link" @click="() => back()">返回</a-button>
-      </template>
 
-      <div>
-        <Log v-if="modelRef.logs" :logs="modelRef.logs"></Log>
+
+
+      <div class="scenario-detail-main">
+        <a-card :bordered="false" :bodyStyle="{paddingTop: '8px'}">
+          <div>
+            <Log v-if="modelRef.logs" :logs="modelRef.logs"></Log>
+          </div>
+        </a-card>
       </div>
     </a-card>
   </div>
 </template>
+
+
 
 <script setup lang="ts">
 import {defineComponent, computed, ref, reactive, ComputedRef} from "vue";
@@ -71,11 +85,6 @@ const get = async (id: number): Promise<void> => {
 }
 const id = ref(+router.currentRoute.value.params.id)
 get(id.value)
-
-const back = ():void =>  {
-  router.replace(`/report/index`)
-}
-
 
 const columns = [
   {
@@ -134,8 +143,8 @@ const data = [
 
 <style lang="less" scoped>
 .scenario-detail-main {
-  .ant-card-body {
-    padding-top: 0 !important;
-  }
+.ant-card-body {
+  padding-top: 0 !important;
+}
 }
 </style>

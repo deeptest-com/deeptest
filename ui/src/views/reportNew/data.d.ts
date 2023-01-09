4554eccdaf44325a6eed10b1cd6e5c33cb1b8ed2
yangxi <yangxi@nancal.com>
2023-01-09 21:37:58 +0800

    commit

--- ui/src/views/reportNew/data.d.ts
+++ ui/src/views/reportNew/data.d.ts
@@ -2,12 +2,34 @@ export interface Report {
   id: number;
   name: string;
   desc: string;
+  passRequestNum: number;
+  failRequestNum: number;
+  totalRequestNum: number;
+  logs: ReportLog[];
 }
+
+
+
+export interface ReportLog{
+  id: number;
+  name: string;
+  resultStatus: string;
+  logs: ReportLogDetail[];
+
+}
+
+export interface ReportLogDetail{
+  name: string;
+  resultStatus: string;
+  exectime: number;
+}
+
 
 export interface QueryResult {
   list: Report[];
   pagination: PaginationConfig;
 }
+
 
 export interface QueryParams {
   keywords:  string,
@@ -16,6 +38,7 @@ export interface QueryParams {
   pageSize: number,
 }
 
+// 分页设置
 export interface PaginationConfig {
   total: number;
   current: number;
--- ui/src/views/reportNew/detail/index.vue
+++ ui/src/views/reportNew/detail/index.vue
@@ -1,62 +1,34 @@
 <template>
-  <div class="report-main">
+  <div>
     <a-card :bordered="false">
-      <div>
-        <a-page-header
-            class="demo-page-header"
-            style="border: 1px solid rgb(235, 237, 240)"
-            title="测试报告-{{ modelRef.name }}"
-            @back="() => $router.go(-1)"
-        >
-          <a-descriptions size="small" :column="1">
-            <a-descriptions-item label="Created" >Lili Qu</a-descriptions-item>
-            <a-descriptions-item label="Association">
-              <a>421421</a>
-            </a-descriptions-item>
-            <a-descriptions-item label="Creation Time">2017-01-10</a-descriptions-item>
-            <a-descriptions-item label="Effective Time">2017-10-10</a-descriptions-item>
-            <a-descriptions-item label="Remarks">
-              Gonghu Road, Xihu District, Hangzhou, Zhejiang, China
-            </a-descriptions-item>
+      <div >
+        <a-card :bordered="false" :bodyStyle="{paddingTop: '8px'}">
+        <a-row>
+          <a-col>测试报告--{{report.name}}</a-col>
+          <a-col>
+          <a-descriptions size="small" :column="4">
+            <a-descriptions-item label="Count" color="green" > {{report.totalRequestNum}}</a-descriptions-item>
+            <a-descriptions-item label="Passed" color="green"> {{report.passRequestNum}}</a-descriptions-item>
+            <a-descriptions-item label="Failed" color="red">{{failCount}}</a-descriptions-item>
+            <a-descriptions-item label="Excute Time">{{}}</a-descriptions-item>
           </a-descriptions>
-        </a-page-header>
-
-            <a-button key="2" type="primary" display="display">全部收起</a-button>
-            <a-button key="1" type="primary" display="true">全部展开</a-button>
-
+          </a-col>
+        </a-row>
+            <!-- <a-button key="2" type="primary" display="display" style="float: right">全部收起</a-button>
+            <a-button key="1" type="primary" display="true" style="float: right">全部展开</a-button> -->
+          </a-card>
       </div>
-      <div class="scenario-detail-main">
-        <a-table :columns="columns" :data-source="data">
-          <template #name="{ text }">
-            <a>{{ text }}</a>
-          </template>
-          <template #customTitle>
-      <span>
-        <smile-outlined />
-        Name
-      </span>
-          </template>
-          <template #tags="{ text: tags }">
-      <span>
-        <a-tag
-            v-for="tag in tags"
-            :key="tag"
-            :color="tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'"
-        >
-          {{ tag.toUpperCase() }}
-        </a-tag>
-      </span>
-          </template>
+
+      <a-card :bordered="false" :bodyStyle="{paddingTop: '8px'}">
+      <div>
+        <a-table :columns="columns" bordered :data-source="data" :pagination=false :rowKey="(record: any) => record.id">
         </a-table>
       </div>
-
-
-
-
-      <div class="scenario-detail-main">
+    </a-card>
+      <div class="scenario-detail-main" >
         <a-card :bordered="false" :bodyStyle="{paddingTop: '8px'}">
           <div>
-            <Log v-if="modelRef.logs" :logs="modelRef.logs"></Log>
+            <Log v-if="report.logs" :logs="report.logs"></Log>
           </div>
         </a-card>
       </div>
@@ -70,72 +42,60 @@
 import {defineComponent, computed, ref, reactive, ComputedRef} from "vue";
 import {useRouter} from "vue-router";
 import {useStore} from "vuex";
+import {BackTop} from "ant-design-vue";
 import { useI18n } from "vue-i18n";
 import {StateType} from "../store";
-import {Report} from "@/views/report/data";
+import {Report} from "@/views/reportNew/data";
+import Log from "../Log.vue"
+import { getAdditionalMeta } from "@intlify/core-base";
 const router = useRouter();
 const { t } = useI18n();
-import Log from "@/views/scenario/exec/components/Log.vue"
+
 
 const store = useStore<{ Report: StateType }>();
-const modelRef = computed<Report>(() => store.state.Report.detailResult);
-
+//千万注意async/await的顺序问题
+const report = computed<Report>(() => store.state.Report.detailResult);
+const id = ref(+router.currentRoute.value.params.id)
 const get = async (id: number): Promise<void> => {
   await store.dispatch('Report/get', id);
 }
-const id = ref(+router.currentRoute.value.params.id)
 get(id.value)
+
+
+const passCount = Number(store.state.Report.detailResult.passRequestNum)
+const totalRequestNum = Number(store.state.Report.detailResult.totalRequestNum)
+const failCount = totalRequestNum-passCount
+const data = ref(store.state.Report.detailResult.logs[0].logs)
 
 const columns = [
   {
+    title: '序号',
+    dataIndex: 'id',
+    key:'rowIndex',
+    width:80,
+    align:"center",
+    customRender: ({ index }: { index: number }) => {
+            return `${index + 1}`
+          }
+  },
+  {
+    title: '接口名称',
     dataIndex: 'name',
     key: 'name',
-    slots: { title: 'customTitle', customRender: 'name' },
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: '运行时间',
+    dataIndex: 'runtime',
+    width:120,
+    align:"center",
+    key: 'runtime',
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    slots: { customRender: 'tags' },
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    slots: { customRender: 'action' },
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
+    title: '执行结果',
+    width:140,
+    align:"center",
+    key: 'resultStatus',
+    dataIndex: 'resultStatus',
   },
 ];
 

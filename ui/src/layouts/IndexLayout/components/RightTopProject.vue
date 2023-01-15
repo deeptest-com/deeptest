b89e9e3e24fba3e04399fea62bc0cca606cfd106
aaron <462826@qq.com>
2023-01-15 15:39:06 +0800

    improvement

--- ui/src/layouts/IndexLayout/components/RightTopProject.vue
+++ ui/src/layouts/IndexLayout/components/RightTopProject.vue
@@ -12,48 +12,31 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import {computed, ComputedRef, defineComponent, onMounted} from "vue";
 import {useStore} from "vuex";
+import router from '@/config/routes';
 import {StateType as UserStateType} from "@/store/user";
 import {StateType as ProjectStateType} from "@/store/project";
 import {StateType as EnvironmentStateType} from "@/store/environment";
 
-interface RightTopProject {
-  message: ComputedRef<number>;
+const store = useStore<{ User: UserStateType, ProjectGlobal: ProjectStateType, Environment: EnvironmentStateType }>();
 
-  projects: ComputedRef<any>;
-  currProject: ComputedRef<any>;
-  selectProject: (value) => void;
+const message = computed<number>(() => store.state.User.message);
+const projects = computed<any>(() => store.state.ProjectGlobal.projects);
+const currProject = computed<any>(() => store.state.ProjectGlobal.currProject);
+
+store.dispatch("User/fetchMessage");
+store.dispatch("ProjectGlobal/fetchProject");
+
+const selectProject = (value): void => {
+  console.log('selectProject', value)
+  store.dispatch('ProjectGlobal/changeProject', value);
+  store.dispatch('Environment/getEnvironment', {id: 0, projectId: value})
+
+  if(router.currentRoute.value.path.indexOf('/scenario/') > -1) {
+    router.replace('/scenario/index')
+  }
 }
 
-export default defineComponent({
-  name: 'RightTopProject',
-  components: {},
-  setup(): RightTopProject {
-    const store = useStore<{ User: UserStateType, ProjectGlobal: ProjectStateType, Environment: EnvironmentStateType }>();
-
-    const message = computed<number>(() => store.state.User.message);
-    const projects = computed<any>(() => store.state.ProjectGlobal.projects);
-    const currProject = computed<any>(() => store.state.ProjectGlobal.currProject);
-
-    store.dispatch("User/fetchMessage");
-    store.dispatch("ProjectGlobal/fetchProject");
-
-    const selectProject = (value): void => {
-      console.log('selectProject', value)
-      store.dispatch('Project/changeProject', value);
-
-      store.dispatch('Environment/getEnvironment', {id: 0, projectId: value})
-    }
-
-    return {
-      message,
-
-      currProject,
-      projects,
-      selectProject,
-    }
-  }
-})
 </script>

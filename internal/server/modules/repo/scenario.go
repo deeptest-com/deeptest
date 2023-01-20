7ff5c063ebf501dc23c75a83bd5fe7ec9614eda4
aaron <462826@qq.com>
2023-01-20 17:56:19 +0800

    scenario category maintenance

--- internal/server/modules/repo/scenario.go
+++ internal/server/modules/repo/scenario.go
@@ -25,7 +25,7 @@ func NewScenarioRepo() *ScenarioRepo {
 func (r *ScenarioRepo) Paginate(req v1.ScenarioReqPaginate, projectId int) (data _domain.PageData, err error) {
 	var count int64
 
-	ids, err := r.BaseRepo.GetAllParentIds(req.CategoryId, model.ScenarioCategory{}.TableName())
+	ids, err := r.BaseRepo.GetAllChildIds(req.CategoryId, model.ScenarioCategory{}.TableName())
 	if err != nil {
 		return
 	}

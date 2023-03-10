36d535a4131a2b23c771bec3f2c22865da345db5
yangxi <yangxi@nancal.com>
2023-03-10 17:58:35 +0800

    modify

--- internal/server/modules/repo/summary-bugs.go
+++ internal/server/modules/repo/summary-bugs.go
@@ -12,6 +12,7 @@ type SummaryBugsRepo struct {
 func NewSummaryBugsRepo() *SummaryBugsRepo {
 	return &SummaryBugsRepo{}
 }
+
 func (r *SummaryBugsRepo) Create(bugs interface{}) (err error) {
 	err = r.DB.Model(&model.SummaryBugs{}).Create(bugs).Error
 	return
@@ -19,27 +20,22 @@ func (r *SummaryBugsRepo) Create(bugs interface{}) (err error) {
 
 func (r *SummaryBugsRepo) CountByProjectId(projectId int64) (count int64, err error) {
 	var bugsCount int64
-	err = r.DB.Model(&model.SummaryBugs{}).Select("count(id)").Where("project_id = ? ", projectId).Find(&bugsCount).Error
+	err = r.DB.Model(&model.SummaryBugs{}).Select("count(id)").Where("project_id = ? AND NOT deleted AND not disabled", projectId).Find(&bugsCount).Error
 	count = bugsCount
 	return
 }
 
 func (r *SummaryBugsRepo) Count() (count int64, err error) {
-	var bugsCount int64
-	err = r.DB.Model(&model.SummaryBugs{}).Select("count(id)").Find(&bugsCount).Error
+	err = r.DB.Model(&model.SummaryBugs{}).Select("count(id) ").Where("NOT deleted AND not disabled").Find(&count).Error
 	return
 }
 
-func (r *SummaryBugsRepo) FindByProjectIdGroupByBugSeverity(projectId int64) (summaryBugs []model.SummaryBugs, err error) {
-	var bugsSeveritySummary []struct {
-		BugSeverity string `gorm:column:bug_severity`
-	}
-
-	err = r.DB.Model(&model.SummaryBugs{}).Select("count(id)").Where("project_id = ? ", projectId).Group("bug_severity").Find(&bugsSeveritySummary).Error
+func (r *SummaryBugsRepo) FindByProjectIdGroupByBugSeverity(projectId int64) (result []model.SummaryBugsSeverity, err error) {
+	err = r.DB.Model(&model.SummaryBugs{}).Select("count(id) as count,bug_severity as severity ").Where("project_id = ? AND NOT deleted AND not disabled", projectId).Group("bug_severity").Find(&result).Error
 	return
 }
 
-func (r *SummaryBugsRepo) FindGroupByBugSeverity() (summaryBugs []model.SummaryBugs, err error) {
-	err = r.DB.Model(&model.SummaryBugs{}).Select("count(id)").Group("bug_severity").Find(&summaryBugs).Error
+func (r *SummaryBugsRepo) FindGroupByBugSeverity() (result []model.SummaryBugsSeverity, err error) {
+	err = r.DB.Model(&model.SummaryBugs{}).Select("count(id) as count,bug_severity as severity").Where(" NOT deleted AND not disabled").Group("bug_severity").Find(&result).Error
 	return
 }

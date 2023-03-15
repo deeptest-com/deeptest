b02a82a66f8598cc6645e15ebaf99bdbc59cee23
yangxi <yangxi@nancal.com>
2023-03-15 20:16:38 +0800

    update

--- internal/server/modules/model/summary-details.go
+++ internal/server/modules/model/summary-details.go
@@ -1,25 +1,44 @@
 package model
 
-import (
-	"time"
-)
+type UserList struct {
+	UserId   int64  `json:"user_id"`
+	UserName string `json:"user_name"`
+}
 
 type SummaryDetails struct {
 	BaseModel
 
-	ProjectId            int64      `json:"project_id"`
-	ProjectName          string     `gorm:"type:text" json:"project_name"`
-	ProjectChineseName   string     `gorm:"type:text" json:"project_chinese_name"`
-	ScenarioTotal        int64      `json:"scenario_total"`
-	InterfaceTotal       int64      `json:"interface_total"`
-	ExecTotal            int64      `json:"exec_total"`
-	PassRate             int        `json:"pass_rate"`
-	Coverage             int        `json:"coverage"`
-	AdminUser            string     `gorm:"type:text" json:"admin_user"`
-	ProjectCreateTime    *time.Time `json:"project_create_time"`
-	ProjectUserRankingId int        `json:"project_user_ranking_id"`
+	ProjectId          int64   `json:"project_id"`
+	ProjectName        string  `gorm:"type:text" json:"project_name"`
+	ProjectChineseName string  `gorm:"type:text" json:"project_chinese_name"`
+	ScenarioTotal      int64   `json:"scenario_total"`
+	InterfaceTotal     int64   `json:"interface_total"`
+	ExecTotal          int64   `json:"exec_total"`
+	PassRate           float64 `json:"pass_rate"`
+	Coverage           float64 `json:"coverage"`
+	AdminUser          string  `gorm:"type:text" json:"admin_user"`
+	ProjectCreateTime  string  `gorm:"type:text" json:"project_create_time"`
+	ProjectDesc        string  `gorm:"type:text" json:"project_desc"`
+	UserList           string  `gorm:"type:text;" json:"user_list"`
 
 	Logs []*ExecLogProcessor `gorm:"-" json:"logs"`
+}
+
+type SummaryCard struct {
+	SummaryCardTotal
+	ProjectTotal int64   `json:"project_total"`
+	InterfaceHB  float64 `json:"interface_hb"`
+	ScenarioHB   float64 `json:"scenario_hb"`
+	CoverageHB   float64 `json:"coverage_hb"`
+}
+
+type SummaryCardTotal struct {
+	ScenarioTotal  int64               `gorm:"column:scenario_total" json:"scenario_total"`
+	InterfaceTotal int64               `gorm:"column:interface_total" json:"interface_total"`
+	ExecTotal      int64               `gorm:"column:exec_total" json:"exec_total"`
+	PassRate       float64             `gorm:"column:pass_rate" json:"pass_rate"`
+	Coverage       float64             `gorm:"column:coverage" json:"coverage"`
+	Logs           []*ExecLogProcessor `gorm:"-" json:"logs"`
 }
 
 func (SummaryDetails) TableName() string {
--- internal/server/modules/repo/summary-details.go
+++ internal/server/modules/repo/summary-details.go
@@ -2,7 +2,6 @@ package repo
 
 import (
 	"github.com/aaronchen2k/deeptest/internal/server/modules/model"
-	"go/types"
 	"gorm.io/gorm"
 )
 
@@ -14,22 +13,57 @@ func NewSummaryDetailsRepo() *SummaryDetailsRepo {
 	return &SummaryDetailsRepo{}
 }
 
-func (r *SummaryDetailsRepo) FindByProjectId(projectId uint) (summaryDetails []model.SummaryDetails, err error) {
-	err = r.DB.Model(&model.SummaryDetails{}).Where("project_id = ?", projectId).Find(&summaryDetails).Error
+func (r *SummaryDetailsRepo) CreateByDate(summaryDetails model.SummaryDetails) (err error) {
+	err = r.DB.Model(&model.SummaryDetails{}).Create(&summaryDetails).Error
+	return
+}
+
+func (r *SummaryDetailsRepo) UpdateColumnsByDate(summaryDetails model.SummaryDetails, startTime string, endTime string) (err error) {
+	err = r.DB.Model(&model.SummaryDetails{}).Where("project_id = ? and created_at > ? and created_at < ?", summaryDetails.ProjectId, startTime, endTime).UpdateColumns(&summaryDetails).Error
+	return
+}
+
+func (r *SummaryDetailsRepo) Count() (count int64, err error) {
+	err = r.DB.Model(&model.Project{}).Select("count(id) ").Where("NOT deleted AND not disabled").Find(&count).Error
+	return
+}
+
+func (r *SummaryDetailsRepo) CountByUserId(userId int64) (count int64, err error) {
+	err = r.DB.Model(&model.ProjectMember{}).Select("count(distinct project_id)").Where("user_id = ? AND NOT deleted AND not disabled", userId).Find(&count).Error
+	return
+}
+
+func (r *SummaryDetailsRepo) FindProjectIdsByUserId(userId int64) (projectIds []int64, err error) {
+	err = r.DB.Model(&model.ProjectMember{}).Select("distinct project_id").Where("user_id = ? AND NOT deleted AND not disabled", userId).Find(&projectIds).Error
+	return
+}
+
+func (r *SummaryDetailsRepo) FindByProjectId(projectId int64) (summaryDetail model.SummaryDetails, err error) {
+	err = r.DB.Model(&model.SummaryDetails{}).Where("project_id = ? AND NOT deleted AND not disabled", projectId).Last(&summaryDetail).Error
 	return
 }
 
 func (r *SummaryDetailsRepo) Find() (summaryDetails []model.SummaryDetails, err error) {
-	err = r.DB.Model(&model.SummaryDetails{}).Find(&summaryDetails).Error
+	err = r.DB.Model(&model.SummaryDetails{}).Raw("select * from (deeptest.biz_summary_details) where id in (SELECT max(id) FROM deeptest.biz_summary_details group by project_id) AND NOT deleted AND not disabled;").Find(&summaryDetails).Error
 	return
 }
 
-func (r *SummaryDetailsRepo) UpdateByProjectId(projectId uint, key string, value types.Object) (err error) {
-	err = r.DB.Update(key, value).Where("project_id= ? ", projectId).Error
+func (r *SummaryDetailsRepo) FindByProjectIds(projectIds []int64) (summaryDetails []model.SummaryDetails, err error) {
+	err = r.DB.Model(&model.SummaryDetails{}).Raw("select * from (deeptest.biz_summary_details) where id in (SELECT max(id) FROM deeptest.biz_summary_details where project_id in ? group by project_id) AND NOT deleted AND not disabled;", projectIds).Find(&summaryDetails).Error
 	return
 }
 
-func (r *SummaryDetailsRepo) Create(summaryDetails model.SummaryDetails) (err error) {
-	err = r.DB.Model(&model.SummaryDetails{}).Create(summaryDetails).Error
+func (r *SummaryDetailsRepo) SummaryCard() (summaryCardTotal []model.SummaryCardTotal, err error) {
+	err = r.DB.Model(&model.SummaryDetails{}).Raw("select SUM(scenario_total) as scenario_total,sum(interface_total) as interface_total,sum(exec_total) as exec_total,AVG(pass_rate) as pass_rate,AVG(coverage) as coverage from (deeptest.biz_summary_details) where id in (SELECT max(id) FROM deeptest.biz_summary_details where NOT deleted AND not disabled group by project_id);").Find(&summaryCardTotal).Error
+	return
+}
+
+func (r *SummaryDetailsRepo) SummaryCardByDate(startTime string, endTime string) (summaryDetails []model.SummaryDetails, err error) {
+	err = r.DB.Model(&model.SummaryDetails{}).Raw("select SUM(scenario_total) as scenario_total,sum(interface_total) as interface_total,sum(exec_total) as exec_total,AVG(pass_rate) as pass_rate,AVG(coverage) as coverage from (deeptest.biz_summary_details) where id in (SELECT max(id) FROM deeptest.biz_summary_details where created_at > ? and created_at < ? AND NOT deleted AND not disabled group by project_id);", startTime, endTime).Find(&summaryDetails).Error
+	return
+}
+
+func (r *SummaryDetailsRepo) FindByProjectIdAndDate(startTime string, endTime string, projectId int64) (summaryDetails model.SummaryDetails, err error) {
+	err = r.DB.Model(&model.SummaryDetails{}).Raw("select * from (deeptest.biz_summary_details) where id in (SELECT max(id) FROM deeptest.biz_summary_details where project_id = ? and created_at > ? and created_at < ? AND NOT deleted AND not disabled group by project_id);", projectId, startTime, endTime).Find(&summaryDetails).Error
 	return
 }

0ded1770ae4c8960478f4b9a3a8751c3b115f441
aaron <462826@qq.com>
2023-04-16 15:06:35 +0800

    save and extract response

--- ui/src/views/component/debug/response/Renderer.vue
+++ ui/src/views/component/debug/response/Renderer.vue
@@ -62,13 +62,7 @@ const responseData = computed<any>(() => store.state.Debug.responseData);
 const extractorsData = computed<any>(() => store.state.Debug.extractorsData);
 const checkpointsData = computed<any>(() => store.state.Debug.checkpointsData);
 
-const title = ref(t('text'))
-
-watch(responseData, () => {
-  console.log('watch responseData')
-  title.value = t(responseData.value.contentLang ? responseData.value.contentLang : 'empty')
-}, {deep: true})
-
+const title = computed(() => t(responseData.value.contentLang ? responseData.value.contentLang : 'empty'))
 const activeKey = ref('1');
 
 const extractorFail = computed(() => {

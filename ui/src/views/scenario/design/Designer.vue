269fb05d1a3b2583ca9ae5844bb2ed14a2c48fec
aaron <462826@qq.com>
2023-04-25 20:25:19 +0800

    show interface in scenario

--- ui/src/views/scenario/design/Designer.vue
+++ ui/src/views/scenario/design/Designer.vue
@@ -5,71 +5,23 @@
       <a-button href="#/scenario/index" type="link" class="dp-btn-group">返回</a-button>
     </div>
 
-    <div id="scenario-design-content" class="dp-splits-v">
-      <div id="scenario-design-content-left">
-        <Processor></Processor>
-      </div>
-
-      <div id="scenario-design-content-splitter" class="splitter"></div>
-
-      <div id="scenario-design-content-right">
-        <a-tabs v-model:activeKey="tabKey"
-                tabPosition="right"
-                :tabBarGutter="0"
-                class="right-tab">
-
-          <a-tab-pane key="env">
-            <template #tab>
-              <a-tooltip placement="left" overlayClassName="dp-tip-small">
-                <template #title>环境</template>
-                <EnvironmentOutlined/>
-              </a-tooltip>
-            </template>
-            <RequestEnv></RequestEnv>
-          </a-tab-pane>
-
-          <a-tab-pane key="history">
-            <template #tab>
-              <a-tooltip placement="left" overlayClassName="dp-tip-small">
-                <template #title>历史</template>
-                <HistoryOutlined/>
-              </a-tooltip>
-            </template>
-            <RequestHistory></RequestHistory>
-          </a-tab-pane>
-
-          <a-tab-pane key="datapool">
-            <template #tab>
-              <a-tooltip placement="left" overlayClassName="dp-tip-small">
-                <template #title>数据池</template>
-                <TableOutlined />
-              </a-tooltip>
-            </template>
-            <Datapool></Datapool>
-          </a-tab-pane>
-
-        </a-tabs>
-      </div>
+    <div class="scenario-design-content">
+      <Processor></Processor>
     </div>
   </div>
 
 </template>
 
 <script setup lang="ts">
-import {computed, ComputedRef, defineComponent, onMounted, PropType, provide, Ref, ref, watch} from "vue";
+import {onMounted, provide, ref} from "vue";
 import {useI18n} from "vue-i18n";
-import {Form, message} from 'ant-design-vue';
-import { HistoryOutlined, EnvironmentOutlined, TableOutlined } from '@ant-design/icons-vue';
+import {Form} from 'ant-design-vue';
 import {useStore} from "vuex";
 
 import {StateType} from "@/views/interface1/store";
-import {resizeWidth} from "@/utils/dom";
 import {useRouter} from "vue-router";
 import {UsedBy} from "@/utils/enum";
 
-import RequestEnv from '@/views/interface1/components/designer/others/env/index.vue';
-import RequestHistory from '@/views/interface1/components/designer/others/history/index.vue';
-import Datapool from '@/views/interface1/components/designer/others/datapool/index.vue';
 import Processor from './components/Processor.vue';
 
 const router = useRouter();
@@ -80,7 +32,6 @@ const {t} = useI18n();
 const store = useStore<{ Interface: StateType }>();
 
 const id = ref(+router.currentRoute.value.params.id)
-const tabKey = ref('env')
 
 const exec = () => {
   router.push(`/scenario/exec/${id.value}`)
@@ -88,47 +39,9 @@ const exec = () => {
 
 onMounted(() => {
   console.log('onMounted')
-  resize()
 })
 
-const resize = () => {
-  resizeWidth('scenario-design-content',
-      'scenario-design-content-left', 'scenario-design-content-splitter', 'scenario-design-content-right', 500, 300)
-}
-
 </script>
-
-<style lang="less">
-.right-tab {
-  height: 100%;
-
-  .ant-tabs-left-content {
-    padding-left: 0px;
-  }
-  .ant-tabs-right-content {
-    padding-right: 0px;
-    height: 100%;
-    .ant-tabs-tabpane {
-      height: 100%;
-      &.ant-tabs-tabpane-inactive {
-        display: none;
-      }
-    }
-  }
-  .ant-tabs-nav-scroll {
-    text-align: center;
-  }
-  .ant-tabs-tab {
-    padding: 5px 10px !important;
-    .anticon {
-      margin-right: 2px !important;
-    }
-  }
-  .ant-tabs-ink-bar {
-    background-color: #d9d9d9 !important;
-  }
-}
-</style>
 
 <style lang="less" scoped>
 #scenario-design-main {
@@ -141,20 +54,9 @@ const resize = () => {
     text-align: right;
   }
 
-  #scenario-design-content {
+  .scenario-design-content {
     height: calc(100% - 32px);
     width: 100%;
-
-    #scenario-design-content-left {
-      flex: 1;
-      width: 0;
-      height: 100%;
-    }
-
-    #scenario-design-content-right {
-      width: 320px;
-      height: 100%;
-    }
   }
 }
 

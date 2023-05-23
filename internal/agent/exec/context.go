ffe84cabf10d00aa8ad592c7846352738eb774a3
aaron <462826@qq.com>
2023-05-23 21:24:43 +0800

    pre-condition scripts

--- internal/agent/exec/context.go
+++ internal/agent/exec/context.go
@@ -60,13 +60,16 @@ func listCachedVariable(processorId uint) (variables []domain.ExecVariable) {
 	effectiveScopeIds := ScopeHierarchy[processorId]
 
 	if effectiveScopeIds == nil {
+		effectiveScopeIds = &[]uint{uint(0)}
+	}
+
+	if effectiveScopeIds == nil {
 		return
 	}
 
 	for _, id := range *effectiveScopeIds {
 		for _, vari := range ScopedVariables[id] {
 			if vari.Scope == consts.Public || (vari.Scope == consts.Private && id == processorId) {
-
 				variables = append(variables, vari)
 			}
 		}

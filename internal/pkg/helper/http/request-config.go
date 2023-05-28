5bc858b35a55c8515f3caae22301003403992fa4
aaron <462826@qq.com>
2023-05-28 14:07:31 +0800

    refactoring

--- internal/pkg/helper/http/request-config.go
+++ internal/pkg/helper/http/request-config.go
@@ -11,12 +11,6 @@ import (
 func DealwithQueryParams(req domain.BaseRequest, httpReq *http.Request) {
 	queryParams := url.Values{}
 
-	for _, p := range req.QueryParams {
-		if p.Name != "" {
-			queryParams.Add(p.Name, p.Value)
-		}
-	}
-
 	for _, pair := range strings.Split(httpReq.URL.RawQuery, "&") {
 		arr := strings.Split(pair, "=")
 		if len(arr) > 1 {
@@ -24,18 +18,26 @@ func DealwithQueryParams(req domain.BaseRequest, httpReq *http.Request) {
 		}
 	}
 
+	for _, p := range req.QueryParams {
+		name := strings.ToUpper(p.Name)
+
+		if name != "" && queryParams.Get(name) == "" {
+			queryParams.Add(name, p.Value)
+		}
+	}
+
 	httpReq.URL.RawQuery = queryParams.Encode()
 }
 
 func DealwithHeader(req domain.BaseRequest, httpReq *http.Request) {
+	httpReq.Header.Set("User-Agent", consts.UserAgentChrome)
+	httpReq.Header.Set("Origin", "DEEPTEST")
+
 	for _, h := range req.Headers {
-		if h.Name != "" {
+		if h.Name != "" && httpReq.Header.Get(h.Name) == "" {
 			httpReq.Header.Set(h.Name, h.Value)
 		}
 	}
-
-	httpReq.Header.Set("User-Agent", consts.UserAgentChrome)
-	httpReq.Header.Set("Origin", "DEEPTEST")
 
 	addAuthorInfo(req, httpReq)
 }

240f3173dd4ca5e4e3f556d5af620e0dcc204847
liguangwei007 <liguwe@qq.com>
2023-06-01 11:30:09 +0800

    feat: 动态执行场景的处理逻辑抽到 hooks 中

---
+++ ui/src/composables/useExecLogs.ts
@@ -0,0 +1,261 @@
+/**
+ * 根据【场景执行】、【测试计划】执行产出的 [动态log]， 生成按场景编排的执行记录树
+ * 1. 生成场景执行记录树
+ * 2. 生成场景执行动态结果数据
+ */
+import {computed, ComputedRef, onMounted, ref, Ref, watch} from 'vue';
+import {getDivision, getPercentStr} from "@/utils/number";
+
+// 打平的执行记录
+const execLogs: any = ref([]);
+// 场景的执行结果列表
+const execRes: any = ref([]);
+
+// 更新场景的执行结果
+// todo 优化: 可以优化成算法，使用 hash
+function updateExecRes(res) {
+    // 1. 更新执行结果
+    if (execRes.value.some((item: any) => item.scenarioId === res.scenarioId)) {
+        execRes.value.forEach((item: any) => {
+            if (item.scenarioId === res.scenarioId) {
+                item = {...item, ...res};
+            }
+        });
+        // 2. 新增执行结果
+    } else {
+        execRes.value.push(res);
+    }
+}
+
+/**
+ * @description 生成执行记录树
+ * @param execLogs 场景的执行日志 数组
+ * @param execRes  场景的执行结果
+ * */
+function genLogTreeView(execLogs, execRes) {
+    // 用于存储根节点，即场景节点,即 processorCategory为 processor_root的节点
+    const scenarioReports: any = [];
+    execLogs.forEach((item: any) => {
+        if (item.processorCategory === "processor_root") {
+            const res = execRes.find((log) => log.scenarioId === item.scenarioId) || {};
+            item.logs = [{
+                ...item,
+                ...res,
+                logs: []
+            }];
+            scenarioReports.push(item);
+        }
+    });
+    scenarioReports.forEach((scenario) => {
+        function fn(array, rootId) {
+            const res: any = [];
+            // 用于存储 树节点的 map
+            const map = {};
+            array.forEach((item) => {
+                map[item.logId] = {...item}
+            });
+            array.forEach((item) => {
+                const {logId, parentLogId} = item;
+                const mapItem = map[logId];
+                if (!mapItem) return;
+                if (!parentLogId) return;
+                if (parentLogId === rootId) {
+                    res.push(mapItem);
+                } else {
+                    if (map[parentLogId]?.logs) {
+                        map[parentLogId].logs.push(mapItem);
+                    } else {
+                        map[parentLogId].logs = [mapItem];
+                    }
+                }
+            })
+            return res;
+        }
+
+        scenario.logs[0].logs = fn(execLogs, scenario.logId);
+    });
+    return scenarioReports;
+}
+
+// 更新场景的执行记录，不包括场景的执行结果
+// todo 优化: 可以优化成算法，使用 hash
+function updateExecLogs(log) {
+    const isExist = execLogs.value.some((item: any) => {
+        return item.logId === log.logId && item.scenarioId === log.scenarioId;
+    });
+    // 1. 更新执行记录
+    if (isExist) {
+        execLogs.value.forEach((item: any) => {
+            if (item.logId === log.logId && item.scenarioId === log.scenarioId) {
+                item = {...item, ...log};
+            }
+        });
+        // 2. 新增执行记录
+    } else {
+        execLogs.value.push(log);
+    }
+}
+
+
+const scenarioReports = computed(() => {
+    return [...genLogTreeView(execLogs.value, execRes.value)];
+})
+
+const expandKeys = computed(() => {
+    return scenarioReports.value.map((item: any) => item.key);
+})
+
+
+// 统计聚合数据
+const statInfo = ref({
+    failAssertionNum: 0,
+    failInterfaceNum: 0,
+    failRequestNum: 0,
+    failScenarioNum: 0,
+    passAssertionNum: 0,
+    passInterfaceNum: 0,
+    passRequestNum: 0,
+    passScenarioNum: 0,
+    totalAssertionNum: 0,
+    totalInterfaceNum: 0,
+    totalRequestNum: 0,
+    totalScenarioNum: 0,
+    totalProcessorNum: 0,
+    notTestNum: 0,
+    finishProcessorNum: 0,
+    duration: 0,
+    cost:0,
+})
+
+const statisticData = computed(() => {
+    const {
+        failAssertionNum,
+        failInterfaceNum,
+        failRequestNum,
+        failScenarioNum,
+        passAssertionNum,
+        passInterfaceNum,
+        passRequestNum,
+        passScenarioNum,
+        totalAssertionNum,
+        totalInterfaceNum,
+        totalRequestNum,
+        totalScenarioNum,
+        totalProcessorNum,
+        duration,
+        notTestNum,
+        cost,
+    } = statInfo.value;
+    // 计算平均接口耗时
+    let interfaceDuration = 0;
+    let interfaceNum = 0;
+    execLogs.value.forEach((item: any) => {
+        if (item.processorCategory === "processor_interface") {
+            interfaceDuration += (item.cost || 0);
+            interfaceNum++;
+        }
+    });
+    const passRate = getPercentStr(passAssertionNum, totalAssertionNum);
+    const notPassRate = getPercentStr(failAssertionNum, totalAssertionNum);
+    const notTestNumRate = getPercentStr(notTestNum, totalAssertionNum);
+    // 平均接口耗时
+    const avgInterfaceDuration = getDivision(interfaceDuration, interfaceNum);
+    return [
+        {
+            label: '通过',
+            value: `${passRate} ${passAssertionNum} 个`,
+        },
+        {
+            label: '接口总耗时',
+            value: `${interfaceDuration} 毫秒`
+        },
+        {
+            label: '失败',
+            value: `${notPassRate}  ${failAssertionNum} 个`,
+            class: 'fail',
+        },
+        {
+            label: '平均接口耗时',
+            value: `${avgInterfaceDuration} 毫秒`,
+        },
+        {
+            label: '未测',
+            value: `${notTestNumRate} ${notTestNum}个`,
+            class: 'fail',
+        },
+        // {
+        //     label: '测试场景 (成功/失败)',
+        //     value: `${totalScenarioNum} (${passScenarioNum}/${failScenarioNum})`,
+        // },
+    ]
+})
+
+/**
+ * 从每次返回的执行日志中更新统计数据
+ * */
+function updateStatFromLog(res: any) {
+    const {
+        failAssertionNum = 0,
+        failInterfaceNum = 0,
+        failRequestNum = 0,
+        failScenarioNum = 0,
+        passAssertionNum = 0,
+        passInterfaceNum = 0,
+        passRequestNum = 0,
+        passScenarioNum = 0,
+        totalAssertionNum = 0,
+        totalInterfaceNum = 0,
+        totalRequestNum = 0,
+        totalScenarioNum = 0,
+        totalProcessorNum = 0,
+        finishProcessorNum = 0,
+        duration = 0,
+        cost = 0,
+    }: any = res;
+    console.log('updateStatFromLog', res);
+    const notTestNum = totalAssertionNum - passAssertionNum - failAssertionNum;
+    statInfo.value = {
+        failAssertionNum,
+        failInterfaceNum,
+        failRequestNum,
+        failScenarioNum,
+        passAssertionNum,
+        passInterfaceNum,
+        passRequestNum,
+        passScenarioNum,
+        totalAssertionNum,
+        totalInterfaceNum,
+        totalRequestNum,
+        totalScenarioNum,
+        totalProcessorNum,
+        finishProcessorNum,
+        notTestNum: notTestNum >= 0 ? notTestNum : 0,
+        duration,
+        cost,
+    }
+}
+
+// 更新统计聚合数据
+function initData(res: any) {
+    updateStatFromLog(res);
+}
+
+// 【计划】的执行最终结果 用于更新最终的执行结果
+function updatePlanRes(res) {
+    updateStatFromLog(res);
+}
+
+const progressStatus = ref('in_progress');
+const progressValue = computed(() => {
+    const {
+        totalProcessorNum = 1,
+        finishProcessorNum,
+    } = statInfo.value;
+    return Math.round(((finishProcessorNum || execLogs.value.length) / totalProcessorNum) * 100);
+});
+
+export {
+    scenarioReports, expandKeys,statInfo,
+    execLogs, execRes, updateExecLogs, updateExecRes
+    , statisticData, initData, progressStatus, progressValue, updatePlanRes,
+};
--- ui/src/views/scenario/components/Exec/index.vue
+++ ui/src/views/scenario/components/Exec/index.vue
@@ -2,7 +2,7 @@
   <div class="scenario-exec-info-main">
     <ReportBasicInfo :items="baseInfoList || []"
                      :showBtn="true"
-                      :btnText="'生成报告'"
+                     :btnText="'生成报告'"
                      @handleBtnClick="genReport"/>
     <StatisticTable :data="statisticData" :value="statInfo"/>
     <Progress :exec-status="progressStatus"
@@ -35,6 +35,11 @@ import {getToken} from "@/utils/localToken";
 import {Scenario} from "@/views/scenario/data";
 import {message} from "ant-design-vue";
 import {getDivision, getPercentStr} from "@/utils/number";
+import {
+  scenarioReports, expandKeys,
+  execLogs, execRes, updateExecLogs, updateExecRes,statInfo
+  , statisticData, initData, progressStatus, progressValue, updatePlanRes,
+} from '@/composables/useExecLogs';
 
 const {t} = useI18n();
 
@@ -57,135 +62,7 @@ const baseInfoList = computed(() => {
     {value: detailResult?.value?.name || '暂无', label: '场景名称'},
     {value: curEnv?.name || '暂无', label: '执行环境'},
   ]
-})
-const statisticData = computed(() => {
-  const {
-    failAssertionNum,
-    passAssertionNum,
-    totalAssertionNum,
-    notTestNum,
-  } = statInfo.value;
-  // 计算平均接口耗时
-  let interfaceDuration = 0;
-  let interfaceNum = 0;
-  execLogs.value.forEach((item: any) => {
-    if (item.processorCategory === "processor_interface") {
-      interfaceDuration += (item.cost || 0);
-      interfaceNum++;
-    }
-  });
-  const passRate = getPercentStr(passAssertionNum, totalAssertionNum);
-  const notPassRate = getPercentStr(failAssertionNum, totalAssertionNum);
-  const notTestNumRate = getPercentStr(notTestNum, totalAssertionNum);
-  // 平均接口耗时
-  const avgInterfaceDuration = getDivision(interfaceDuration, interfaceNum);
-  return [
-    {
-      label: '通过',
-      value: `${passRate} ${passAssertionNum} 个`,
-    },
-    {
-      label: '接口总耗时',
-      value: `${interfaceDuration} 毫秒`
-    },
-    {
-      label: '失败',
-      value: `${notPassRate}  ${failAssertionNum} 个`,
-      class: 'fail',
-    },
-    {
-      label: '平均接口耗时',
-      value: `${avgInterfaceDuration} 毫秒`,
-    },
-    {
-      label: '未测',
-      value: `${notTestNumRate} ${notTestNum}个`,
-      class: 'fail',
-    },
-  ]
-})
-const progressValue = computed(() => {
-  const {
-    totalProcessorNum = 1,
-    finishProcessorNum,
-  } = statInfo.value;
-  return Math.round(((finishProcessorNum || execLogs.value.length) / totalProcessorNum) * 100);
 });
-const progressStatus = ref('in_progress');
-// 统计聚合数据
-const statInfo = ref({
-  failAssertionNum: 0,
-  failInterfaceNum: 0,
-  failRequestNum: 0,
-  failScenarioNum: 0,
-  passAssertionNum: 0,
-  passInterfaceNum: 0,
-  passRequestNum: 0,
-  passScenarioNum: 0,
-  totalAssertionNum: 0,
-  totalScenarioNum: 0,
-  totalProcessorNum: 0,
-  notTestNum: 0,
-  finishProcessorNum: 0,
-  cost:0,
-});
-
-const scenarioReports = computed(() => {
-  return [...genLogTreeView(execLogs.value, execRes.value)];
-})
-
-const expandKeys = computed(() => {
-  return scenarioReports.value.map((item: any) => item.key);
-})
-
-/**
- * @description 生成执行记录树
- * @param execLogs 场景的执行日志 数组
- * @param execRes  场景的执行结果
- * */
-function genLogTreeView(execLogs, execRes) {
-  // 用于存储根节点，即场景节点,即 processorCategory为 processor_root的节点
-  const scenarioReports: any = [];
-  execLogs.forEach((item: any) => {
-    if (item.processorCategory === "processor_root") {
-      const res = execRes.find((log) => log.scenarioId === item.scenarioId) || {};
-      item.logs = [{
-        ...item,
-        ...res,
-        logs: []
-      }];
-      scenarioReports.push(item);
-    }
-  });
-  scenarioReports.forEach((scenario) => {
-    function fn(array, rootId) {
-      const res: any = [];
-      // 用于存储 树节点的map
-      const map = {};
-      array.forEach((item) => {
-        map[item.id] = {...item}
-      });
-      array.forEach((item) => {
-        const {id, parentId} = item;
-        if (!map[id]) return;
-        if (!parentId) return;
-        if (parentId === rootId) {
-          res.push(map[id]);
-        } else {
-          if (map[parentId]?.logs) {
-            map[parentId].logs.push(map[id]);
-          } else {
-            map[parentId].logs = [map[id]];
-          }
-        }
-      })
-      return res;
-    }
-    scenario.logs[0].logs = fn(execLogs, scenario.id);
-  });
-  console.log('832 scenarioReports', scenarioReports)
-  return scenarioReports;
-}
 
 const execStart = async () => {
   const data = {
@@ -212,95 +89,10 @@ onUnmounted(() => {
   bus.off(settings.eventWebSocketMsg, OnWebSocketMsg);
 })
 
-// 打平的执行记录
-const execLogs: any = ref([]);
-// 场景的执行结果列表
-const execRes: any = ref([]);
-
-// 更新场景的执行结果
-// todo 优化: 可以优化成算法，使用 hash
-function updateExecRes(res) {
-  // 1. 更新执行结果
-  if (execRes.value.some((item: any) => item.scenarioId === res.scenarioId)) {
-    execRes.value.forEach((item: any) => {
-      if (item.scenarioId === res.scenarioId) {
-        item = {...item, ...res};
-      }
-    });
-    // 2. 新增执行结果
-  } else {
-    execRes.value.push(res);
-  }
-}
-
-// 更新场景的执行记录，不包括场景的执行结果
-// todo 优化: 可以优化成算法，使用 hash
-function updateExecLogs(log) {
-  const isExist = execLogs.value.some((item: any) => {
-    return item.logId === log.logId && item.scenarioId === log.scenarioId;
-  });
-  // 1. 更新执行记录
-  if (isExist) {
-    execLogs.value.forEach((item: any) => {
-      if (item.logId === log.logId && item.scenarioId === log.scenarioId) {
-        item = {...item, ...log};
-      }
-    });
-    // 2. 新增执行记录
-  } else {
-    execLogs.value.push(log);
-  }
-}
-
-/**
- * 从每次返回的执行日志中更新统计数据
- * */
-function updateStatFromLog(res: any) {
-  const {
-    failAssertionNum = 0,
-    failInterfaceNum = 0,
-    failRequestNum = 0,
-    failScenarioNum = 0,
-    passAssertionNum = 0,
-    passInterfaceNum = 0,
-    passRequestNum = 0,
-    passScenarioNum = 0,
-    totalAssertionNum = 0,
-    totalInterfaceNum = 0,
-    totalRequestNum = 0,
-    totalScenarioNum = 0,
-    totalProcessorNum = 0,
-    finishProcessorNum = 0,
-    cost = 0,
-  }: any = res;
-  console.log('updateStatFromLog', res);
-  const notTestNum = totalAssertionNum - passAssertionNum - failAssertionNum;
-  statInfo.value = {
-    failAssertionNum,
-    failInterfaceNum,
-    failRequestNum,
-    failScenarioNum,
-    passAssertionNum,
-    passInterfaceNum,
-    passRequestNum,
-    passScenarioNum,
-    totalAssertionNum,
-    totalScenarioNum,
-    totalProcessorNum,
-    finishProcessorNum,
-    cost,
-    notTestNum: notTestNum >= 0 ? notTestNum : 0,
-  }
-}
-
-// 更新统计聚合数据
-function initData(res: any) {
-  updateStatFromLog(res);
-}
 
 const OnWebSocketMsg = (data: any) => {
   if (!data.msg) return;
-  if(progressStatus.value === 'cancel') return;
+  if (progressStatus.value === 'cancel') return;
   const wsMsg = JSON.parse(data.msg);
   const log = wsMsg.data ? JSON.parse(JSON.stringify(wsMsg.data)) : {};
   // 开始执行，初始化数据
@@ -329,14 +121,13 @@ const OnWebSocketMsg = (data: any) => {
   }
 }
 
-
 async function genReport() {
   const res = await store.dispatch('Scenario/genReport', {
     id: reportId.value,
   });
-  if(res){
+  if (res) {
     message.success('生成报告成功');
-  }else {
+  } else {
     message.error('生成报告失败');
   }
 }

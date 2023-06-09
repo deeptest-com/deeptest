6aba3a17f69ccc327e3586fd1874b68b4e3b37c0
codewang <309742371@163.com>
2023-06-09 17:24:59 +0800

    接口文档

--- internal/server/modules/service/document.go
+++ internal/server/modules/service/document.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	domain "github.com/aaronchen2k/deeptest/cmd/server/v1/domain"
+	"github.com/aaronchen2k/deeptest/internal/server/modules/model"
 )
 
 type DocumentService struct {
@@ -9,4 +10,17 @@ type DocumentService struct {
 
 func (s *DocumentService) Content(req domain.DocumentReq) (res domain.DocumentReq, err error) {
 	return
+}
+
+func (s *DocumentService) GetInterfaces(projectId uint, serves, endpointIds []uint) (uint, []uint, []model.Endpoint) {
+	var endpoints []model.Endpoint
+	if projectId != 0 {
+
+	} else if len(serves) != 0 {
+
+	} else if len(endpointIds) != 0 {
+
+	}
+
+	return projectId, serves, endpoints
 }

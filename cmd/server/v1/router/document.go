1016ec56ea63ac7042e229daee8479c7ab663223
codewang <309742371@163.com>
2023-06-09 16:36:33 +0800

    接口文档

---
+++ cmd/server/v1/router/document.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/aaronchen2k/deeptest/cmd/server/v1/handler"
+	"github.com/aaronchen2k/deeptest/internal/pkg/core/module"
+	"github.com/kataras/iris/v12"
+)
+
+type DocumentModule struct {
+	DocumentCtrl *handler.DocumentCtrl `inject:""`
+}
+
+func (m *DocumentModule) Party() module.WebModule {
+	handler := func(public iris.Party) {
+		//public.Use(middleware.InitCheck(), middleware.JwtHandler(), middleware.OperationRecord(), middleware.Casbin())
+		public.Get("/", m.DocumentCtrl.Index).Name = "接口文档"
+
+	}
+	return module.NewModule("/document", handler)
+}
---
+++ internal/server/modules/service/document.go
@@ -0,0 +1,12 @@
+package service
+
+import (
+	domain "github.com/aaronchen2k/deeptest/cmd/server/v1/domain"
+)
+
+type DocumentService struct {
+}
+
+func (s *DocumentService) Content(req domain.DocumentReq) (res domain.DocumentReq, err error) {
+	return
+}

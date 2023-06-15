a1f5b9597bf8e84749eaf5cd30405ba7bb1325ac
zhourui <2228097305@qq.com>
2023-06-15 18:23:55 +0800

    接口数据导入

---
+++ internal/pkg/helper/openapi/convert/swaggerV3.go
@@ -0,0 +1,27 @@
+package convert
+
+import (
+	"encoding/json"
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+type SwaggerV3 struct {
+	driver
+	doc openapi3.T
+}
+
+func newSwaggerV3() *SwaggerV3 {
+	return new(SwaggerV3)
+}
+
+func (d *SwaggerV3) toOpenapi() (doc *openapi3.T, err error) {
+	doc = &d.doc
+	return
+}
+
+func (d *SwaggerV3) Doc(data []byte) {
+	err := json.Unmarshal(data, &d.doc)
+	if err != err {
+		panic(err)
+	}
+}

25edc448d988fe79da9fbb6c63f0844fe89481a6
aaron <462826@qq.com>
2023-06-19 10:56:01 +0800

    fix a bug of loading debug interface data

--- internal/server/modules/model/test-interface.go
+++ internal/server/modules/model/test-interface.go
@@ -30,6 +30,7 @@ type TestInterface struct {
 	// debug data
 	InterfaceConfigBase
 
+	Url         string                `json:"url"`
 	BaseUrl     string                `json:"baseUrl"`
 	QueryParams []TestInterfaceParam  `gorm:"-" json:"queryParams"`
 	PathParams  []TestInterfaceParam  `gorm:"-" json:"pathParams"`

db0fb0551277a5396a76259f1b55ac1bb1eabc95
aaron <462826@qq.com>
2023-06-27 16:26:34 +0800

    fix checkpoints issues

--- ui/src/views/component/debug/response/Renderer/Checkpoint.vue
+++ ui/src/views/component/debug/response/Renderer/Checkpoint.vue
@@ -103,7 +103,7 @@
             <a-textarea v-model:value="model.expression" :auto-size="{ minRows: 2, maxRows: 5 }"
                      @blur="validate('expression', { trigger: 'blur' }).catch(() => {})" />
 
-            <div class="dp-input-tip">{{t('tips_expression_bool')}}</div>
+            <div class="dp-input-tip">{{t('tips_expression_bool', {name: '{name}'})}}</div>
           </a-form-item>
 
           <a-form-item v-if="model.type !== 'judgement'" label="数值" v-bind="validateInfos.value">

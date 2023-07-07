22435b4202319fde3dcf93d75fd0a4d96c9c3e37
codewang <309742371@163.com>
2023-07-07 15:03:28 +0800

    配置文件路径

--- cmd/server/v1/router/document.go
+++ cmd/server/v1/router/document.go
@@ -13,10 +13,10 @@ type DocumentModule struct {
 
 func (m *DocumentModule) Party() module.WebModule {
 	handler := func(public iris.Party) {
+		public.Use(middleware.InitCheck(), middleware.JwtHandler(), middleware.OperationRecord(), middleware.Casbin())
 		public.Post("/share", m.DocumentCtrl.GetShareLink).Name = "生成分享接口文档的链接"
 		public.Get("/get_share_content", m.DocumentCtrl.GetContentsByShareLink).Name = "查看分享的文档"
 		public.Get("/share_detail", m.DocumentCtrl.GetDocumentDetail).Name = "查看分享的文档详情"
-		public.Use(middleware.InitCheck(), middleware.JwtHandler(), middleware.OperationRecord(), middleware.Casbin())
 		public.Post("/", m.DocumentCtrl.Index).Name = "接口文档"
 		public.Post("/version_list", m.DocumentCtrl.DocumentVersionList).Name = "接口文档版本列表"
 		public.Post("/publish", m.DocumentCtrl.Publish).Name = "发布接口文档"

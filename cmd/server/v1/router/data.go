c2b3dc04fd1c0b4de697b7dffc95b22f7f8ee0b3
codewang <309742371@163.com>
2023-07-11 14:39:34 +0800

    初始db

--- cmd/server/v1/router/data.go
+++ cmd/server/v1/router/data.go
@@ -1,13 +1,16 @@
 package router
 
 import (
+	serverDomain "github.com/aaronchen2k/deeptest/cmd/server/v1/domain"
 	"github.com/aaronchen2k/deeptest/cmd/server/v1/handler"
 	"github.com/aaronchen2k/deeptest/internal/pkg/core/module"
+	"github.com/aaronchen2k/deeptest/internal/server/modules/service"
 	"github.com/kataras/iris/v12"
 )
 
 type DataModule struct {
-	DataCtrl *handler.DataCtrl `inject:""`
+	DataCtrl    *handler.DataCtrl    `inject:""`
+	DataService *service.DataService `inject:""`
 }
 
 // Party 初始化模块
@@ -16,5 +19,8 @@ func (m *DataModule) Party() module.WebModule {
 		index.Post("/initdb", m.DataCtrl.Init)
 		index.Get("/checkdb", m.DataCtrl.Check)
 	}
+
+	m.DataService.InitDB(serverDomain.DataReq{ClearData: true})
+	
 	return module.NewModule("/init", handler)
 }

2128a3c2a5d3b34258dd7dec0dbe0e982f663957
liguangwei <liguwe@qq.com>
2023-07-11 16:20:13 +0800

    feat: 接口调试页面支持拖动左侧目录树

--- ui/src/views/diagnose/index.vue
+++ ui/src/views/diagnose/index.vue
@@ -1,59 +1,17 @@
 <template>
-  <div id="diagnose-interface-main" class="dp-splits-v">
-    <div id="diagnose-interface-left" v-if="!collapsed">
-      <DiagnoseInterfaceTree />
-    </div>
-
-    <CollapsedIcon
-        :style="{left:'294px',top:'300px'}"
-        :collapsedStyle="{left:'-9px', top:'300px'}"
-        @click="collapsed = !collapsed" :collapsed="collapsed" />
-
-    <div id="diagnose-interface-right">
-      <DiagnoseInterfaceDesign />
-    </div>
-  </div>
+  <ContentPane>
+    <template #left>
+      <DiagnoseInterfaceTree/>
+    </template>
+    <template #right>
+      <DiagnoseInterfaceDesign/>
+    </template>
+  </ContentPane>
 </template>
 
-
 <script setup lang="ts">
-import {computed, ref} from "vue";
-import {useStore} from "vuex";
-import CollapsedIcon from "@/components/CollapsedIcon/index.vue"
-
 import DiagnoseInterfaceTree from './components/tree.vue';
 import DiagnoseInterfaceDesign from './design/index.vue';
-
-import {StateType as DiagnoseInterfaceStateType} from "@/views/diagnose/store";
-import {StateType as ProjectStateType} from "@/store/project";
-import {StateType as ServeStateType} from "@/store/serve";
-
-const collapsed = ref(false);
-
-const store = useStore<{ DiagnoseInterface: DiagnoseInterfaceStateType, ProjectGlobal: ProjectStateType, ServeGlobal: ServeStateType }>();
-const currProject = computed<any>(() => store.state.ProjectGlobal.currProject);
-const currServe = computed<any>(() => store.state.ServeGlobal.currServe);
-
+import ContentPane from '@/views/component/ContentPane/index.vue';
 </script>
 
-<style lang="less" scoped>
-#diagnose-interface-main {
-  display: flex;
-  height: 100%;
-  margin: 16px;
-  position: relative;
-
-  #diagnose-interface-left {
-    width: 300px;
-    height: 100%;
-    background-color: #ffffff;
-    border-right: 1px solid #f0f0f0;
-  }
-  #diagnose-interface-right {
-    flex: 1;
-    width: 0;
-    height: 100%;
-    background-color: #ffffff;
-  }
-}
-</style>

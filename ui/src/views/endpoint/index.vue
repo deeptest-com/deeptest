586d4ba04cc0c002d6a3b2c05a82d2912cab8a0f
zhourui <2228097305@qq.com>
2023-07-12 14:09:20 +0800

    批量修改

--- ui/src/views/endpoint/index.vue
+++ ui/src/views/endpoint/index.vue
@@ -438,6 +438,8 @@ async function handleImport(data, callback) {
 const filterState:any = ref({});
 async function selectNode(id) {
   selectedCategoryId.value = id;
+  selectedRowKeys. value = [];
+  selectedRow.value = {};
   // 选中节点时，重置分页为第一页
   await loadList(1, pagination.value.pageSize, {
     categoryId: id,

a9779d622f12f071cedd6c1a8f53bc9d05df9d59
liguangwei <liguwe@qq.com>
2023-07-12 16:04:12 +0800

    feat: 删除编排无用代码

--- ui/src/views/scenario/components/Design/index.vue
+++ ui/src/views/scenario/components/Design/index.vue
@@ -13,7 +13,6 @@
 import {computed, defineProps, onMounted, onUnmounted, ref, watch} from "vue";
 import {useRouter} from "vue-router";
 
-import {resizeWidth} from "@/utils/dom";
 import {useStore} from "vuex";
 
 import {StateType as GlobalStateType} from "@/store/global";
@@ -21,42 +20,16 @@ import {StateType as ScenarioStateType} from "../../store";
 
 import ScenarioTree from "./Tree.vue"
 import Designer from "./Designer.vue"
-import {Scenario} from "@/views/scenario/data";
 import ContentPane from '@/views/component/ContentPane/index.vue';
 
 const router = useRouter();
 const store = useStore<{ Scenario: ScenarioStateType; Global: GlobalStateType; }>();
-// const detailResult = computed<Scenario>(() => store.state.Scenario.detailResult);
-// const id = ref(+router.currentRoute.value.params.id)
-// const props = defineProps({
-//   id: {
-//     required: true,
-//     type: Number,
-//   },
-// })
 
-const collapsed = computed<boolean>(() => store.state.Global.collapsed);
 
 onMounted(() => {
   console.log('onMounted')
-  resize()
   store.dispatch('Scenario/getNode', null)
 })
-
-onUnmounted(() => {
-  console.log('onUnmounted')
-})
-
-watch(collapsed, () => {
-  console.log('watch collapsed')
-  resize()
-}, {deep: true})
-
-const resize = () => {
-  resizeWidth('scenario-design-main',
-      'scenario-design-left', 'scenario-design-splitter', 'scenario-design-right',
-      260, 800)
-}
 
 </script>
 

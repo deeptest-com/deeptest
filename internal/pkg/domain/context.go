3b28eae04ea3e089e3a2cefea0824897a1fab015
zhourui <2228097305@qq.com>
2023-07-20 15:49:57 +0800

    环境变量的bug

--- internal/pkg/domain/context.go
+++ internal/pkg/domain/context.go
@@ -3,11 +3,11 @@ package domain
 import "github.com/aaronchen2k/deeptest/internal/pkg/consts"
 
 type GlobalVar struct {
-	VarId          uint   `gorm:"-" json:"varId"`
+	VarId       uint   `gorm:"-" json:"varId"`
 	Name        string `json:"name"`
-	RightValue  string `json:"rightValue"`
-	LocalValue  string `json:"localValue"`
-	RemoteValue string `json:"remoteValue"`
+	RightValue  string `gorm:"type:text" json:"rightValue"`
+	LocalValue  string `gorm:"type:text" json:"localValue"`
+	RemoteValue string `gorm:"type:text" json:"remoteValue"`
 }
 type GlobalParam struct {
 	Name         string           `json:"name"`

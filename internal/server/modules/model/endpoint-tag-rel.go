b0e869b6ac646e5024881d97733cdb8404d4457d
zhourui <2228097305@qq.com>
2023-07-21 11:02:48 +0800

    tag

--- internal/server/modules/model/endpoint-tag-rel.go
+++ internal/server/modules/model/endpoint-tag-rel.go
@@ -2,8 +2,10 @@ package model
 
 type EndpointTagRel struct {
 	BaseModel
-	EndpointId uint `json:"endpointId"`
-	TagId      uint `json:"tagId"`
+	EndpointId uint   `gorm:"index:endpoint_tag_project_index,unique;not null;type:varchar(200)" json:"endpointId"`
+	TagId      uint   `json:"tagId"`
+	TagName    string `gorm:"index:endpoint_tag_project_index,unique;not null;type:varchar(200)" json:"tagName"`
+	ProjectId  uint   `gorm:"index:endpoint_tag_project_index,unique;not null;type:varchar(200)" json:"projectId"`
 }
 
 func (EndpointTagRel) TableName() string {

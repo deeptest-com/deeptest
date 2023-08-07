a214a86b37dbf8395424d040bba4065585dc7d72
liguangwei <liguwe@qq.com>
2023-08-07 17:52:14 +0800

    feat: 合并数据迭代器

--- ui/src/views/scenario/components/Design/components/proccessors/data/default.vue
+++ ui/src/views/scenario/components/Design/components/proccessors/data/default.vue
@@ -1,62 +1,71 @@
 <template>
   <div class="processor_data_excel-main">
     <a-card :bordered="false">
-      <div>
-        <a-form :label-col="labelCol" :wrapper-col="wrapperCol">
-
-          <a-form-item label="变量名称" v-bind="validateInfos.variableName">
-            <a-input v-model:value="modelRef.variableName"
-                     @blur="validate('variableName', { trigger: 'blur' }).catch(() => {})"/>
-
-            <div v-if="modelRef.variableName" class="dp-input-tip">
-              可使用 {{'${' + modelRef.variableName + '.列名' + '}'}} 访问数据变量
-            </div>
-          </a-form-item>
-
-          <a-form-item label="上传文件" v-bind="validateInfos.url">
-            <div v-if="isElectron" class="upload-file-by-electron">
-              <a-input v-model:value="modelRef.url" readonly="readonly" />
-              <a-button @click="uploadFile()">
-                <UploadOutlined />
-              </a-button>
-            </div>
-
-            <div v-else class="upload-file">
-              <div class="input-container">
-                <a-input v-model:value="modelRef.url" readonly="readonly" />
-              </div>
-              <div class="upload-container">
-                <a-upload :beforeUpload="upload"
-                          :showUploadList="false"
-                          accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">
-                  <a-button>
-                    <UploadOutlined />
-                  </a-button>
-                </a-upload>
-              </div>
-            </div>
-          </a-form-item>
-
-          <a-form-item label="重复次数" v-bind="validateInfos.repeatTimes">
-            <a-input-number v-model:value="modelRef.repeatTimes"
-                     @blur="validate('repeatTimes', { trigger: 'blur' }).catch(() => {})"/>
-          </a-form-item>
-
-          <a-form-item label="是否随机" v-bind="validateInfos.isRand">
-            <a-switch v-model:checked="modelRef.isRand"
-                     @blur="validate('isRand', { trigger: 'blur' }).catch(() => {})"/>
-          </a-form-item>
-
-          <a-form-item label="备注" v-bind="validateInfos.comments">
-            <a-input v-model:value="modelRef.comments"/>
-          </a-form-item>
-
-          <a-form-item :wrapper-col="{ span: 16, offset: 4 }">
-            <a-button type="primary" @click.prevent="submitForm">保存</a-button>
-            <a-button style="margin-left: 10px" @click="resetFields">重置</a-button>
-          </a-form-item>
-        </a-form>
+      <div class="top-header-tip">
+<!--        <strong>说明：</strong>-->
+<!--        <span>数据迭代处理器将循环读取文件中的行内容，并将读取的内容赋值给指定的变量</span>-->
+        <a-alert message="说明：数据迭代处理器将循环读取文件中的行内容，并将读取的内容赋值给指定的变量" type="info" show-icon />
       </div>
+
+      <a-form :label-col="labelCol" :wrapper-col="wrapperCol">
+        <a-form-item label="变量名称" v-bind="validateInfos.variableName">
+          <a-input v-model:value="modelRef.variableName"
+                   @blur="validate('variableName', { trigger: 'blur' }).catch(() => {})"/>
+          <div v-if="modelRef.variableName" class="dp-input-tip">
+            可使用 {{ '${' + modelRef.variableName + '.列名' + '}' }} 访问数据变量
+          </div>
+        </a-form-item>
+
+        <a-form-item label="上传文件" v-bind="validateInfos.url">
+          <div v-if="isElectron" class="upload-file-by-electron">
+            <a-input v-model:value="modelRef.url" readonly="readonly"/>
+            <a-button @click="uploadFile()">
+              <UploadOutlined/>
+            </a-button>
+          </div>
+
+          <div v-else class="upload-file">
+            <div class="input-container">
+              <a-input v-model:value="modelRef.url" readonly="readonly"/>
+            </div>
+            <div class="upload-container">
+              <a-upload :beforeUpload="upload"
+                        :showUploadList="false"
+                        accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">
+                <a-button>
+                  <UploadOutlined/>
+                </a-button>
+              </a-upload>
+            </div>
+          </div>
+          <span class="dp-input-tip">仅支持excel、csv、 text三种文件格式</span>
+        </a-form-item>
+
+        <a-form-item label="重复次数" v-bind="validateInfos.repeatTimes">
+          <a-input-number
+              style="width: 200px;"
+              v-model:value="modelRef.repeatTimes"
+              @blur="validate('repeatTimes', { trigger: 'blur' }).catch(() => {})"/>
+          <div class="dp-input-tip">将按指定次数循环读取文件内容</div>
+        </a-form-item>
+
+        <a-form-item label="是否随机" v-bind="validateInfos.isRand">
+          <a-switch v-model:checked="modelRef.isRand"
+                    @blur="validate('isRand', { trigger: 'blur' }).catch(() => {})"/>
+          <div class="dp-input-tip">开关打开，将按照随机顺序读取文件行内容</div>
+        </a-form-item>
+
+        <a-form-item label="备注" v-bind="validateInfos.comments">
+          <a-input v-model:value="modelRef.comments"/>
+        </a-form-item>
+
+
+        <a-form-item :wrapper-col="{ span: 16, offset: 4 }">
+          <a-button type="primary" @click.prevent="submitForm">保存</a-button>
+        </a-form-item>
+      </a-form>
+
+
     </a-card>
   </div>
 </template>
@@ -66,14 +75,14 @@ import {computed, onMounted, onUnmounted, reactive, ref} from "vue";
 import {useRouter} from "vue-router";
 import {useStore} from "vuex";
 import {useI18n} from "vue-i18n";
-import {Form, message, notification} from 'ant-design-vue';
+import {Form, message} from 'ant-design-vue';
 import {StateType as ScenarioStateType} from "../../../../../store";
 import {UploadOutlined} from "@ant-design/icons-vue";
-import {NotificationKeyCommon} from "@/utils/const";
 import settings from "@/config/settings";
 import {getUrls} from "@/utils/request";
 import {getToken} from "@/utils/localToken";
 import {uploadRequest} from "@/utils/upload";
+import IconSvg from "@/components/IconSvg";
 
 const useForm = Form.useForm;
 
@@ -101,15 +110,9 @@ const submitForm = async () => {
       .then(() => {
         store.dispatch('Scenario/saveProcessor', modelRef.value).then((res) => {
           if (res === true) {
-            notification.success({
-              key: NotificationKeyCommon,
-              message: `保存成功`,
-            });
+            message.success('保存成功');
           } else {
-            notification.error({
-              key: NotificationKeyCommon,
-              message: `保存失败`,
-            });
+            message.error('保存失败');
           }
         })
       })
@@ -165,16 +168,16 @@ onUnmounted(() => {
   console.log('onUnmounted')
 })
 
-const labelCol = { span: 4 }
-const wrapperCol = { span: 16 }
+const labelCol = {span: 4}
+const wrapperCol = {span: 16}
 
 </script>
 
 <style lang="less" scoped>
-.processor_data_excel-main {
-  .icons {
-    text-align: right;
-    line-height: 32px;
-  }
+.top-header-tip {
+  position: relative;
+  //left: 63px;
+  margin: 6px  auto 24px 60px;
+  //font-size: 14px;
 }
 </style>

9ce0b1edd8ac8b1b2a3ae9338ea51e4fe796b1a4
liguangwei <liguwe@qq.com>
2023-08-07 18:55:43 +0800

    feat: data循环控制器详情编辑页

--- ui/src/views/scenario/components/Design/components/proccessors/data/default.vue
+++ ui/src/views/scenario/components/Design/components/proccessors/data/default.vue
@@ -2,38 +2,32 @@
   <div class="processor_data_excel-main">
     <a-card :bordered="false">
       <div class="top-header-tip">
-<!--        <strong>说明：</strong>-->
-<!--        <span>数据迭代处理器将循环读取文件中的行内容，并将读取的内容赋值给指定的变量</span>-->
-        <a-alert message="说明：数据迭代处理器将循环读取文件中的行内容，并将读取的内容赋值给指定的变量" type="info" show-icon />
+        <a-alert message="说明：数据迭代处理器将循环读取文件中的行内容，并将读取的内容赋值给指定的变量" type="info" show-icon/>
       </div>
 
-      <a-form :label-col="labelCol" :wrapper-col="wrapperCol">
-        <a-form-item label="变量名称" v-bind="validateInfos.variableName">
-          <a-input v-model:value="modelRef.variableName"
-                   @blur="validate('variableName', { trigger: 'blur' }).catch(() => {})"/>
-          <div v-if="modelRef.variableName" class="dp-input-tip">
-            可使用 {{ '${' + modelRef.variableName + '.列名' + '}' }} 访问数据变量
+      <a-form
+          ref="formRef"
+          :rules="rules"
+          :model="formState"
+          :label-col="{ span: 4 }"
+          @submit.prevent
+          :wrapper-col="{ span: 16 }">
+        <a-form-item label="变量名称" name="variableName">
+          <a-input v-model:value="formState.variableName"/>
+          <div v-if="formState.variableName" class="dp-input-tip">
+            可使用 {{ '${' + formState.variableName + '.列名' + '}' }} 访问数据变量
           </div>
         </a-form-item>
 
-        <a-form-item label="上传文件" v-bind="validateInfos.url">
-          <div v-if="isElectron" class="upload-file-by-electron">
-            <a-input v-model:value="modelRef.url" readonly="readonly"/>
-            <a-button @click="uploadFile()">
-              <UploadOutlined/>
-            </a-button>
-          </div>
-
-          <div v-else class="upload-file">
-            <div class="input-container">
-              <a-input v-model:value="modelRef.url" readonly="readonly"/>
-            </div>
+        <a-form-item label="上传文件" name="url">
+          <div class="upload-file">
             <div class="upload-container">
               <a-upload :beforeUpload="upload"
-                        :showUploadList="false"
-                        accept="application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">
+                        :fileList="fileList"
+                        :showUploadList="true"
+                        :accept="'.xls, .xlsx, .csv, .txt'">
                 <a-button>
-                  <UploadOutlined/>
+                  <UploadOutlined/>上传文件
                 </a-button>
               </a-upload>
             </div>
@@ -41,37 +35,39 @@
           <span class="dp-input-tip">仅支持excel、csv、 text三种文件格式</span>
         </a-form-item>
 
-        <a-form-item label="重复次数" v-bind="validateInfos.repeatTimes">
-          <a-input-number
-              style="width: 200px;"
-              v-model:value="modelRef.repeatTimes"
-              @blur="validate('repeatTimes', { trigger: 'blur' }).catch(() => {})"/>
+        <a-form-item label="分隔符" name="separator">
+          <a-input style="width: 200px;"
+              v-model:value="formState.separator"/>
+          <div class="dp-input-tip">一行多列内容可以使用分隔符来分隔</div>
+        </a-form-item>
+
+        <a-form-item label="重复次数" name="repeatTimes">
+          <a-input-number style="width: 200px;"
+              v-model:value="formState.repeatTimes"/>
           <div class="dp-input-tip">将按指定次数循环读取文件内容</div>
         </a-form-item>
 
-        <a-form-item label="是否随机" v-bind="validateInfos.isRand">
-          <a-switch v-model:checked="modelRef.isRand"
-                    @blur="validate('isRand', { trigger: 'blur' }).catch(() => {})"/>
+        <a-form-item label="是否随机" name="isRand">
+          <a-switch v-model:checked="formState.isRand"/>
           <div class="dp-input-tip">开关打开，将按照随机顺序读取文件行内容</div>
         </a-form-item>
 
-        <a-form-item label="备注" v-bind="validateInfos.comments">
-          <a-input v-model:value="modelRef.comments"/>
+        <a-form-item label="备注" name="comments">
+          <a-input v-model:value="formState.comments"/>
         </a-form-item>
-
 
         <a-form-item :wrapper-col="{ span: 16, offset: 4 }">
-          <a-button type="primary" @click.prevent="submitForm">保存</a-button>
+          <a-button type="primary" @click.prevent="submit">保存</a-button>
         </a-form-item>
-      </a-form>
 
+      </a-form>
 
     </a-card>
   </div>
 </template>
 
 <script setup lang="ts">
-import {computed, onMounted, onUnmounted, reactive, ref} from "vue";
+import {computed, ref, watch} from "vue";
 import {useRouter} from "vue-router";
 import {useStore} from "vuex";
 import {useI18n} from "vue-i18n";
@@ -82,7 +78,6 @@ import settings from "@/config/settings";
 import {getUrls} from "@/utils/request";
 import {getToken} from "@/utils/localToken";
 import {uploadRequest} from "@/utils/upload";
-import IconSvg from "@/components/IconSvg";
 
 const useForm = Form.useForm;
 
@@ -92,92 +87,96 @@ const {t} = useI18n();
 
 const formRef = ref();
 
-const rulesRef = reactive({
+const rules:any = ref({
   variableName: [
     {required: true, message: '请输入变量名称', trigger: 'blur'},
   ],
   url: [
-    {required: true, message: '请输入文件路径', trigger: 'blur'},
+    {required: true, message: '请选择文件', trigger: 'blur'},
   ],
 });
 
+
 const store = useStore<{ Scenario: ScenarioStateType; }>();
-const modelRef = computed<any>(() => store.state.Scenario.nodeData);
-const {resetFields, validate, validateInfos} = useForm(modelRef, rulesRef);
+const nodeData: any = computed<boolean>(() => store.state.Scenario.nodeData);
 
-const submitForm = async () => {
-  validate()
-      .then(() => {
-        store.dispatch('Scenario/saveProcessor', modelRef.value).then((res) => {
-          if (res === true) {
-            message.success('保存成功');
-          } else {
-            message.error('保存失败');
-          }
-        })
-      })
-};
 
-const isElectron = ref(!!window.require)
-let ipcRenderer = undefined as any
-if (isElectron.value && !ipcRenderer) {
-  ipcRenderer = window.require('electron').ipcRenderer
+const formState = ref({
+  variableName: '',
+  url: '',
+  separator: '',
+  repeatTimes: 1,
+  isRand: false,
+  comments: '',
+});
 
-  ipcRenderer.on(settings.electronMsgReplay, (event, result) => {
-    console.log('from electron: ', result)
-    if (result.code === 0) {
-      modelRef.value.url = result.data.path
-    }
-  })
-}
 
-const uploadFile = async () => {
-  console.log('uploadFile')
-
-  if (isElectron.value) {
-    const data = {
-      act: 'uploadFile',
-      url: getUrls().serverUrl + '/upload',
-      token: await getToken(),
-      filters: [
-        {name: 'Excel Files', extensions: ['xlsx']},
-      ]
-    }
-
-    ipcRenderer.send(settings.electronMsg, data)
-  }
-}
-
+const fileList = ref([]);
 const upload = async (file, fileList) => {
-  console.log('upload', file, fileList)
-
+  // debugger;
   const res = await uploadRequest(file)
-  modelRef.value.url = res.path
-
+  formState.value.url = res.path;
+  // debugger;
+  // fileList.value = [{
+  //   ...file,
+  //   status: 'done',
+  //   url: res.path,
+  // }];
   return false
 }
 
-onMounted(() => {
-  console.log('onMounted')
-  if (!modelRef.value.url) modelRef.value.url = ''
-  if (!modelRef.value.variableName) modelRef.value.variableName = ''
-  if (!modelRef.value.repeatTimes) modelRef.value.repeatTimes = 1
-})
+watch(() => {
+  return nodeData.value;
+}, (val: any) => {
+  if (!val) return;
+  formState.value.variableName = val.variableName;
+  formState.value.url = val.url;
+  formState.value.separator = val.separator;
+  formState.value.repeatTimes = val.repeatTimes;
+  formState.value.isRand = val.isRand;
+  formState.value.comments = val.comments;
+});
 
-onUnmounted(() => {
-  console.log('onUnmounted')
-})
+watch(() => {
+  return formState.value.url;
+}, (val: any) => {
+  if (!val) return;
+  //  .txt 结尾
+  if(val.endsWith('.txt')) {
+    rules.value.separator = [
+      {required: true, message: '请输入分隔符', trigger: 'blur'},
+    ]
+  }
 
-const labelCol = {span: 4}
-const wrapperCol = {span: 16}
+
+}, {deep: true});
+
+const submit = async () => {
+  formRef.value
+      .validate()
+      .then(async () => {
+        // 下面代码改成 await 的方式
+        const res = await store.dispatch('Scenario/saveProcessor', {
+          ...nodeData.value,
+          ...formState.value,
+        });
+        if (res === true) {
+          message.success('保存成功');
+        } else {
+          message.error('保存失败');
+        }
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+};
+
 
 </script>
 
 <style lang="less" scoped>
 .top-header-tip {
   position: relative;
-  //left: 63px;
-  margin: 6px  auto 24px 60px;
-  //font-size: 14px;
+  margin: 6px auto 24px 60px;
 }
 </style>

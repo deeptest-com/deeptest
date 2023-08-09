85cd11e213b638d08a2f996c27dca807f60a19e0
qf124g <yxf792812@163.com>
2023-08-09 14:01:38 +0800

    fix: [LYAPI-317]测试开发，列表没有数据的时候，表头展示

--- ui/src/views/scenario/components/List/index.vue
+++ ui/src/views/scenario/components/List/index.vue
@@ -41,22 +41,21 @@
     </div>
 
     <a-table
-        v-if="list.length > 0"
-        row-key="id"
-        :columns="columns"
-        :data-source="list"
-        :loading="loading"
-        :pagination="{
-                ...pagination,
-                onChange: (page) => {
-                    getList(page, nodeDataCategory.id);
-                },
-                onShowSizeChange: (page, size) => {
-                    pagination.pageSize = size
-                    getList(page, nodeDataCategory.id);
-                },
-            }"
-        class="dp-table">
+      row-key="id"
+      :columns="columns"
+      :data-source="list"
+      :loading="loading"
+      :pagination="{
+        ...pagination,
+        onChange: (page) => {
+            getList(page, nodeDataCategory.id);
+        },
+        onShowSizeChange: (page, size) => {
+            pagination.pageSize = size
+            getList(page, nodeDataCategory.id);
+        },
+      }"
+      class="dp-table">
 
       <template #name="{ record ,text }">
         <EditAndShowField :custom-class="'custom-endpoint show-on-hover'"
@@ -122,7 +121,6 @@
         </a-dropdown>
       </template>
     </a-table>
-    <a-empty v-if="list.length === 0" :image="simpleImage"/>
   </div>
 
   <ScenarioCreate :visible="isEditVisible"

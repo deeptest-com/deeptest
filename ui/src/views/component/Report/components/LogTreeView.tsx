ae041513a0ba8afdc0d0704796976242d0987807
liguangwei <liguwe@qq.com>
2023-08-17 16:49:20 +0800

    feat: 除循环迭代之外的执行器联调

--- ui/src/views/component/Report/components/LogTreeView.tsx
+++ ui/src/views/component/Report/components/LogTreeView.tsx
@@ -71,24 +71,28 @@ export default defineComponent({
                 if (log.processorCategory === 'processor_interface') {
                     return <InterfaceContent endpointData={log}/>
                 }
-                return <LogContent data={log}/>;
+                return null;
+                // return <LogContent data={log}/>;
             }
 
             // 渲染场景的标题，主要针对迭代场景，需要标识当前迭代的序号
             function renderCollapseTitle(log, logIndex, srcLog) {
+                const detail = JSON.parse(srcLog.detail || {});
+                const variableName = detail?.variableName;
+
                 // 迭代直到  + 迭代次数 展示文案为 第X轮
                 if (srcLog.processorType === 'processor_loop_time' || srcLog.processorType === 'processor_loop_until') {
-                    return <span class={'collapse-title'}>{`第 ${logIndex + 1} 轮`}</span>
+                    return <span class={'collapse-title'}>{`第 ${logIndex + 1} 次`}</span>
                 }
                 // 迭代列表
                 else if (srcLog.processorType === 'processor_loop_list') {
-                    return <span class={'collapse-title'}>{`${srcLog?.var || 'X'} = ${23}`}</span>
+                    return <span class={'collapse-title'}>{`${variableName} = ${logIndex + 1}`}</span>
                     //  循环区间
                 } else if (srcLog.processorType === 'processor_loop_range') {
-                    return <span class={'collapse-title'}>{`${srcLog?.var || 'X'} = ${23}`}</span>
+                    return <span class={'collapse-title'}>{`${variableName} = ${logIndex + 1}`}</span>
                     // 数据迭代
                 } else if (srcLog.processorType === 'processor_loop_data') {
-                    return <span class={'collapse-title'}>{`${srcLog?.var || 'X'} = ${23}`}</span>
+                    return <span class={'collapse-title'}>{`${variableName} = ${logIndex + 1}`}</span>
                 }
                 return null;
             }
--- ui/src/views/component/Report/components/ProcessorHeader.vue
+++ ui/src/views/component/Report/components/ProcessorHeader.vue
@@ -19,103 +19,82 @@
     <div class="summary">
       <!-- ::::数据迭代 -->
       <template v-if="record.processorType === 'processor_data_default'">
-        <p class="text">从<a  :href="downloadUrl">文件名</a>中{{ `随机` }}读取变量<code>{{ `a` }}</code>， 且重复 <code>{{ 3 }}</code> 次
+        <p class="text">从<a  :href="downloadUrl">文件名</a>中{{ `${detail?.sleepTime}` }}读取变量<code>{{ `${detail?.sleepTime}` }}</code>， 且重复 <code>{{ `${detail?.sleepTime}` }}</code> 次
         </p>
       </template>
       <!-- ::::迭代次数：processor_loop_time -->
       <template v-if="record.processorType === 'processor_loop_time'">
-        <p class="text">迭代<code>{{ 3 }}</code>次</p>
+        <p class="text">迭代<code>{{ `${detail?.times}` }}</code>次</p>
       </template>
       <!-- ::::循环列表 -->
       <template v-if="record.processorType === 'processor_loop_in'">
-        <p class="text">在 <code>{{ `a,b,c,d` }}</code> 中 {{ `随机` }} 读取变量 <code>{{ `a` }}</code></p>
+        <p class="text">在 <code>{{ `${detail?.sleepTime}` }}</code> 中 {{ `${detail?.sleepTime}` }} 读取变量 <code>{{ `${detail?.sleepTime}` }}</code></p>
       </template>
-
       <!-- ::::循环直到 -->
       <template v-if="record.processorType === 'processor_loop_until'">
-        <p class="text">循环直到 <code>{{ `a-b` }}</code> 为  <code>{{ `true` }}</code></p>
+        <p class="text">迭代直到 <code>{{ `${detail?.untilExpression}` }}</code> 为  <code>{{ `true` }}</code>时</p>
       </template>
-
       <!-- ::::循环区间 -->
       <template v-if="record.processorType === 'processor_loop_range'">
-        <p class="text">在区间 <code>{{ `[a-b]` }}</code> 中 {{ `随机` }} 读取变量 <code>{{ `a` }}</code></p>
+        <p class="text">在区间 <code>{{ `[${detail?.range}]` }}</code> 中 <code>{{ `${detail?.isRand ? '顺序' : '随机'}` }}</code> 读取变量 <code>{{ `${detail?.variableName}` }}</code></p>
       </template>
-
       <!-- ::::跳出循环 -->
       <template v-if="record.processorType === 'processor_loop_break'">
-        <p class="text">满足条件 <code>{{ `a-b>0` }}</code> 时， 跳出迭代</p>
+        <p class="text">满足条件 <code>{{ `${detail?.sleepTime}` }}</code> 时， 跳出迭代</p>
       </template>
-
       <!-- ::::条件分支-如果 -->
       <template v-if="record.processorType === 'processor_logic_if'">
-        <p class="text">如果 <code>{{ `a-b` }}</code> 为 <code>true</code></p>
+        <p class="text">如果 <code>{{ `${detail?.expression}` }}</code> 为 <code>true</code></p>
       </template>
-
       <!-- ::::否则 -->
       <template v-if="record.processorType === 'processor_logic_else'">
         <p class="text">否则</p>
       </template>
-
       <!-- ::::等待时间 -->
       <template v-if="record.processorType === 'processor_time_default'">
-        <p class="text">等待 <code>{{ `2` }}</code> 秒</p>
+        <p class="text">等待 <code>{{ `${detail?.sleepTime}` }}</code> 秒</p>
       </template>
-
-
       <!-- ::::设置Cookie -->
       <template v-if="record.processorType === 'processor_cookie_set'">
-        <p class="text">设置 <code>{{ `Cookie1` }}</code> 为 <code>{{ `122` }}</code></p>
+        <p class="text">设置 <code>{{ `${detail?.cookieName}` }}</code> 为 <code>{{ `${detail?.variableValue}` }}</code></p>
       </template>
-
       <!-- ::::清空Cookie -->
       <template v-if="record.processorType === 'processor_cookie_clear'">
-        <p class="text">清除 <code>{{ `Cookie1` }}</code></p>
+        <p class="text">清除 <code>{{ `${detail?.cookieName}` }}</code></p>
       </template>
-
       <!-- ::::设置变量 -->
       <template v-if="record.processorType === 'processor_variable_set'">
-        <p class="text">设置变量 <code>{{ `x` }}</code>为<code>{{ `1` }}</code></p>
+        <p class="text">设置变量 <code>{{ `${detail?.variableName}` }}</code>为<code>{{ `${detail?.variableValue}` }}</code></p>
       </template>
-
-      <!-- ::::删除变量 -->
+      <!-- ::::清除变量 -->
       <template v-if="record.processorType === 'processor_variable_clear'">
-        <p class="text">清除变量<code>{{ `x` }}</code></p>
+        <p class="text">清除变量<code>{{ `${detail?.sleepTime}` }}</code></p>
       </template>
-
       <!-- ::::输出 -->
       <template v-if="record.processorType === 'processor_print_default'">
-        <p class="text">输出：{{ record?.Detail?.['结果'] || '无输出数据' }} x</p>
+        <p class="text">输出：{{ `${detail?.result}` }} </p>
       </template>
-
       <!-- ::::断言 -->
       <template v-if="record.processorType === 'processor_assertion_default'">
-        <p class="text">断言表达式：<code>{{ `a - x` }}</code></p>
+        <p class="text">断言表达式：<code>{{ `${detail?.expression}` }}</code></p>
       </template>
-
       <!-- ::::自定义代码 -->
       <template v-if="record.processorType === 'processor_custom_code'">
-        <p class="text"><code>{{ `hello worold console.log` }}</code></p>
+        <p class="text"><code>{{ `${detail?.content}` }}</code></p>
       </template>
     </div>
-
-
     <div class="status" v-if="showScenarioExecStatus.hasOwnProperty(record.processorType)">
-      <span v-if="true" class="success">{{ showScenarioExecStatus[record.processorType]?.success }}</span>
+      <span v-if="detail?.result" class="success">{{ showScenarioExecStatus[record.processorType]?.success }}</span>
       <span v-else class="fail">{{ showScenarioExecStatus[record.processorType]?.fail }}</span>
     </div>
-
-
     <div class="right" @click.stop="clickMore">
       详情
       <RightOutlined/>
     </div>
-
     <LogContentDrawer
         :data="data"
         :visible="visible"
         @onClose="visible = false"/>
-
-
   </div>
 </template>
 <script setup lang="ts">
@@ -142,6 +121,10 @@ const data = computed(() => {
 
 const downloadUrl = computed(() => {
   return `${window.location.origin}/${props?.record?.datail?.path}`
+})
+
+const detail = computed(() => {
+  return JSON.parse(props.record?.detail || props.record?.Detail || '{}');
 })
 
 function clickMore() {

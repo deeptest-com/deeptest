ffb00ab86b2bce4606bd5090cbe0c98140324ec1
aaron <462826@qq.com>
2023-08-21 15:46:13 +0800

    fix issues

--- internal/agent/exec/domain/stat.go
+++ internal/agent/exec/domain/stat.go
@@ -1,7 +1,7 @@
 package agentDomain
 
 type InterfaceStat struct {
-	InterfaceDurationTotal   int64 `json:"interfaceDurationTotal"` // sec
+	InterfaceDurationTotal   int64 `json:"interfaceDurationTotal"` // milliseconds
 	InterfaceDurationAverage int64 `json:"interfaceDurationAverage"`
 	InterfaceCount           int64 `json:"interfaceCount"`
 

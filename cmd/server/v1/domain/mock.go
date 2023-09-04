4e4c142861b5716ec40bd9ec69338d0023868079
aaron <462826@qq.com>
2023-09-04 11:26:46 +0800

    mock config

--- cmd/server/v1/domain/mock.go
+++ cmd/server/v1/domain/mock.go
@@ -4,3 +4,15 @@ type MockReqJson struct {
 	Name string `json:"name"`
 	Key  string `json:"key"`
 }
+
+type MockJsExpression struct {
+	ID uint `json:"id"`
+
+	Name       string `json:"name"`
+	Expression string `json:"expression"`
+	Format     string `json:"format"`
+	Desc       string `json:"desc"`
+	Ordr       int    `json:"ordr"`
+
+	Result interface{} `gorm:"-" json:"result"`
+}

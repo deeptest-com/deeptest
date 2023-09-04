f774fe337544ba8760f1db55a845c214e58bc81e
aaron <462826@qq.com>
2023-09-04 11:16:40 +0800

    mock config

--- ui/src/layouts/IndexLayout/routes.ts
+++ ui/src/layouts/IndexLayout/routes.ts
@@ -248,7 +248,7 @@ const IndexLayoutRoutes: Array<RoutesDataItem> = [
         component: () => import('@/views/project-settings/components/SwaggerSync/index.vue'),
         hidden: false,
         meta: {
-          code: 'PROJECT-SETTING-MEMBERS'
+          code: 'PROJECT-SETTING-SWAGGER-SYNC'
         }
       },
     ],

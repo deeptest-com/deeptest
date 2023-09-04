1db17ac296622f81d8cb6f16b6ccc3fe75e29eb0
aaron <462826@qq.com>
2023-09-04 18:24:46 +0800

    move mock data api path to http://127.0.0.1:8085/mocks

--- cmd/test/test/sample_test.go
+++ cmd/test/test/sample_test.go
@@ -1,13 +1,56 @@
 package test
 
 import (
-	_intUtils "github.com/aaronchen2k/deeptest/pkg/lib/int"
+	commonUtils "github.com/aaronchen2k/deeptest/pkg/lib/comm"
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/routers/gorillamux"
 	"log"
 	"testing"
 )
 
 func TestSample(t *testing.T) {
-	indexArr := _intUtils.GenUniqueRandNum(0, 10, 10)
+	json := `{
+  "openapi": "3.0.2",
+  "info": {
+    "title": "My other API",
+    "version": "0.1.0"
+  },
+  "components": {
+    "schemas": {
+      "DefaultObject": {
+        "type": "object",
+        "properties": {
+          "foo": {
+            "type": "string"
+          },
+          "bar": {
+            "type": "integer"
+          }
+        }
+      }
+    },
+    "responses": {
+      "DefaultResponse": {
+        "description": "",
+        "content": {
+          "application/json": {
+            "schema": {
+              "$ref": "#/components/schemas/DefaultObject"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+	doc3 := openapi3.T{}
+	commonUtils.JsonDecode(json, &doc3)
 
-	log.Println(indexArr)
+	ret, err := gorillamux.NewRouter(&doc3)
+	if err != nil {
+		return
+	}
+
+	log.Println(ret)
 }

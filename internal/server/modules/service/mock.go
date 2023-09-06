77a512d27a92fa3192c742a0019f5d5f1ed36be8
codewang <309742371@163.com>
2023-09-06 14:50:20 +0800

    mock支持response

--- internal/server/modules/service/mock.go
+++ internal/server/modules/service/mock.go
@@ -8,6 +8,7 @@ import (
 	mockResponder "github.com/aaronchen2k/deeptest/internal/pkg/helper/openapi-mock/openapi/responder"
 	"github.com/aaronchen2k/deeptest/internal/server/modules/model"
 	"github.com/aaronchen2k/deeptest/internal/server/modules/repo"
+	commonUtils "github.com/aaronchen2k/deeptest/pkg/lib/comm"
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/getkin/kin-openapi/routers"
 	"github.com/getkin/kin-openapi/routers/gorillamux"
@@ -79,12 +80,12 @@ func (s *MockService) ByRequest(req *MockRequest, ctx iris.Context) (resp mockGe
 	//}
 
 	// generate response
-	response, err := s.generator.GenerateResponse(&apiRequest, requestRoute)
+	response, err := s.generator.GenerateResponse(&apiRequest, requestRoute, req.Code)
 	if err != nil {
 		return
 	}
 
-	log.Println(resp)
+	log.Println(response)
 
 	resp = *response
 
@@ -160,15 +161,15 @@ func (s *MockService) generateEndpointRouter(endpointId uint) (err error) {
 
 		// init mock router
 	*/
-	//loader := &openapi3.Loader{Context: context.Background(), IsExternalRefsAllowed: true}
-	//x := commonUtils.JsonEncode(doc3)
-	//
-	//doc3, err = loader.LoadFromData([]byte(x))
-	//doc3.Servers = nil
-	pth := "/Users/aaron/rd/project/gudi/deeptest/xdoc/openapi/openapi3/test1.json"
-	ctx := context.Background()
-	loader := &openapi3.Loader{Context: ctx, IsExternalRefsAllowed: true}
-	doc3, err = loader.LoadFromFile(pth)
+	loader := &openapi3.Loader{Context: context.Background(), IsExternalRefsAllowed: true}
+	x := commonUtils.JsonEncode(doc3)
+
+	doc3, err = loader.LoadFromData([]byte(x))
+	doc3.Servers = nil
+	//pth := "/Users/aaron/rd/project/gudi/deeptest/xdoc/openapi/openapi3/test1.json"
+	//ctx := context.Background()
+	//loader := &openapi3.Loader{Context: ctx, IsExternalRefsAllowed: true}
+	//doc3, err = loader.LoadFromFile(pth)
 	ret, err := gorillamux.NewRouter(doc3)
 
 	if err != nil {
@@ -182,13 +183,24 @@ func (s *MockService) generateEndpointRouter(endpointId uint) (err error) {
 
 func (s *MockService) GetEndpointInterface(req *MockRequest) (ret model.EndpointInterface, err error) {
 	if req.EndpointInterfaceId <= 0 {
-		serve, _ := s.ServeRepo.Get(uint(req.ServeId))
-		endpoint, _ := s.EndpointRepo.GetByPath(serve.ID, "/"+req.EndpointPath)
+		var serve model.Serve
+		serve, err = s.ServeRepo.Get(uint(req.ServeId))
+		if err != nil {
+			return
+		}
+		var endpoint model.Endpoint
+		endpoint, err = s.EndpointRepo.GetByPath(serve.ID, "/"+req.EndpointPath)
+		if err != nil {
+			return
+		}
 
 		_, req.EndpointInterfaceId = s.EndpointInterfaceRepo.GetByMethod(endpoint.ID, req.EndpointMethod)
 	}
 
 	ret, err = s.EndpointInterfaceRepo.Get(req.EndpointInterfaceId)
+	if err != nil {
+		return
+	}
 
 	return
 }
@@ -222,7 +234,8 @@ type MockRequest struct {
 	EndpointPath   string            `json:"endpointPath"`
 	EndpointMethod consts.HttpMethod `json:"endpointMethod"`
 
-	EndpointInterfaceId uint `json:"endpointInterfaceId"`
+	EndpointInterfaceId uint   `json:"endpointInterfaceId"`
+	Code                string `json:"code"`
 }
 
 type MockResponse struct {

5d223406b8215983caf92b9a99f7c3538e1fddb3
aaron <462826@qq.com>
2023-09-06 09:11:02 +0800

    write mock data to response

--- internal/server/modules/model/project-mock-settings.go
+++ internal/server/modules/model/project-mock-settings.go
@@ -2,13 +2,16 @@ package model
 
 import (
 	"github.com/aaronchen2k/deeptest/internal/pkg/consts"
+	mockData "github.com/aaronchen2k/deeptest/internal/pkg/helper/openapi-mock/openapi/generator/data"
 )
 
 type ProjectMockSetting struct {
 	BaseModel
 
-	Priority  consts.MockPriority `json:"priority"`
-	ProjectId uint                `json:"projectId"`
+	Priority    consts.MockPriority      `json:"priority"`
+	UseExamples mockData.UseExamplesEnum `gorm:"default:1" json:"useExamplesEnum"`
+
+	ProjectId uint `json:"projectId"`
 }
 
 func (ProjectMockSetting) TableName() string {

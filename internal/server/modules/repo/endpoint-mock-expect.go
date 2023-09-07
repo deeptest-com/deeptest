73f430d3d6bfdd0d2744c3cce5e955b77e7ded66
zhourui <2228097305@qq.com>
2023-09-07 10:02:10 +0800

    mock期望

--- internal/server/modules/repo/endpoint-mock-expect.go
+++ internal/server/modules/repo/endpoint-mock-expect.go
@@ -2,7 +2,11 @@ package repo
 
 import "github.com/aaronchen2k/deeptest/internal/server/modules/model"
 
-func (r *PlanRepo) ListByEndpointId(endpointId uint) (res []model.EndpointMockExpect, err error) {
+type EndpointMockExpectRepo struct {
+	*BaseRepo `inject:""`
+}
+
+func (r *EndpointMockExpectRepo) ListByEndpointId(endpointId uint) (res []model.EndpointMockExpect, err error) {
 	err = r.DB.Model(model.EndpointMockExpect{}).
 		Where("endpoint_id = ?", endpointId).
 		Where("NOT deleted AND NOT disabled").

cec7e42838f05c6ffefb84838273fe523ec6bb0a
codewang <309742371@163.com>
2023-09-20 08:51:40 +0800

    curl导入接口文档不展示名称

--- internal/server/modules/service/endpoint.go
+++ internal/server/modules/service/endpoint.go
@@ -379,13 +379,14 @@ func (s *EndpointService) curlToEndpoint(endpoint *model.Endpoint) (err error) {
 
 	endpoint.Path = curlObj.ParsedURL.Path
 
-	endpoint.Interfaces = s.getInterfaces(curlObj, wf)
+	endpoint.Interfaces = s.getInterfaces(endpoint.Title, curlObj, wf)
 
 	return
 }
 
-func (s *EndpointService) getInterfaces(cURL *curlHelper.CURL, wf *requests.Temporary) (interfaces []model.EndpointInterface) {
+func (s *EndpointService) getInterfaces(name string, cURL *curlHelper.CURL, wf *requests.Temporary) (interfaces []model.EndpointInterface) {
 	interf := model.EndpointInterface{}
+	interf.Name = name
 	interf.Params = s.getQueryParams(wf.GetQuery())
 	interf.Headers = s.getHeaders(wf.Header)
 	interf.Cookies = s.getCookies(wf.Cookies)

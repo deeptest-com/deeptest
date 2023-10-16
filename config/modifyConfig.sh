86ec42d3a920e2730417954a155fd6a5d7628431
leyanapi01 <leyanapi01@nancal.com>
2023-10-16 10:22:02 +0800

    update to new version

---
+++ config/modifyConfig.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  
+  
+# 指定要修改的文件路径  
+file_path="/workspace/leyanapi-backend/internal/pkg/consts/consts.go"  
+  
+# 使用sed命令替换文件中的内容  
+sed -i 's/ConfigFileName = "server.yaml"/ConfigFileName = "\/workspace\/leyanapi-backend\/config\/configMap\/server.yaml"/g' "$file_path"

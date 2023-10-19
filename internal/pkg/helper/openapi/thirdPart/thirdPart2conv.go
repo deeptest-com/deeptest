2ed7507b832c4180968d3045402d36f62df2dfba
wangzhen <wangzhen@nancal.com>
2023-10-19 17:47:52 +0800

    三方生产

--- internal/pkg/helper/openapi/thirdPart/thirdPart2conv.go
+++ internal/pkg/helper/openapi/thirdPart/thirdPart2conv.go
@@ -1,4 +1,16 @@
 package thirdPart
 
+import "github.com/aaronchen2k/deeptest/internal/pkg/helper/openapi"
+
 type thirdPart2conv struct {
+}
+
+func NewThirdPart2conv() *thirdPart2conv {
+	return new(thirdPart2conv)
+}
+
+func (t *thirdPart2conv) Convert(schemas Schemas) (ret openapi.Schema) {
+
+	return
+
 }

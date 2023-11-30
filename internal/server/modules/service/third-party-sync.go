b6774e716e140ec7b582ede082f522ee5b4036a7
zhourui <zhourui@nancal.com>
2023-11-30 18:10:56 +0800

    feat:#LeYanAPI-1293 lecang导入

--- internal/server/modules/service/third-party-sync.go
+++ internal/server/modules/service/third-party-sync.go
@@ -140,7 +140,7 @@ func (s *ThirdPartySyncService) SaveData() (err error) {
 					continue
 				}
 
-				newEndpointDetail, err := s.GenerateEndpoint(endpoint.ID, functionDetail)
+				newEndpointDetail, err := s.GenerateEndpoint(functionDetail)
 				if err != nil && err != gorm.ErrRecordNotFound {
 					continue
 				}
@@ -193,7 +193,7 @@ func (s *ThirdPartySyncService) SaveData() (err error) {
 
 				//}
 
-				endpointId, err := s.SaveEndpoint(title, projectId, serveId, userId, oldEndpointId, int64(categoryId), path, newSnapshot, consts.FullCover)
+				endpointId, err := s.SaveEndpoint(title, projectId, serveId, userId, oldEndpointId, int64(categoryId), path, newSnapshot, consts.AutoAdd)
 				if err != nil {
 					continue
 				}
@@ -359,52 +359,34 @@ func (s *ThirdPartySyncService) GetSchema(bodyString, requestType string) (schem
 
 }
 
-func (s *ThirdPartySyncService) GenerateEndpoint(endpointId uint, functionDetail v1.MetaGetMethodDetailResData) (res model.Endpoint, err error) {
-
-	//res, err = s.EndpointRepo.GetAll(endpointId, "v0.1.0")
-	//if err != nil {
-	//	return
-	//}
-	endpointInterface := model.EndpointInterface{}
-
+func (s *ThirdPartySyncService) GenerateEndpoint(functionDetail v1.MetaGetMethodDetailResData) (res model.Endpoint, err error) {
 	functionBody := functionDetail.RequestBody
 	if functionDetail.RequestType == "FORM" {
 		functionBody = functionDetail.RequestFormBody
 	}
 
-	requestBody := model.EndpointInterfaceRequestBody{}
-	//requestBody := res.Interfaces[0].RequestBody
-
 	requestBodySchema := s.GetSchema(functionBody, functionDetail.RequestType)
 	requestSchemaString, _ := json.Marshal(requestBodySchema)
 
 	requestBodyItem := model.EndpointInterfaceRequestBodyItem{}
-	//requestBodyItem, err := s.EndpointInterfaceRepo.GetRequestBodyItem(requestBody.ID)
-	//if err != nil {
-	//	return
-	//}
-
 	requestBodyItem.Content = string(requestSchemaString)
+
+	requestBody := model.EndpointInterfaceRequestBody{}
 	requestBody.MediaType = s.getBodyType(functionDetail.RequestType).String()
 	requestBody.SchemaItem = requestBodyItem
-
-	responseBody := model.EndpointInterfaceResponseBody{}
-	//responseBody := res.Interfaces[0].ResponseBodies[0]
 
 	responseBodySchema := s.GetSchema(functionDetail.ResponseBody, functionDetail.RequestType)
 	responseSchemaString, _ := json.Marshal(responseBodySchema)
 
 	responseBodyItem := model.EndpointInterfaceResponseBodyItem{}
-	//responseBodyItem, err := s.EndpointInterfaceRepo.GetResponseBodyItem(responseBody.ID)
-	//if err != nil {
-	//	return
-	//}
-
 	responseBodyItem.Content = string(responseSchemaString)
+
+	responseBody := model.EndpointInterfaceResponseBody{}
 	responseBody.MediaType = s.getBodyType(functionDetail.RequestType).String()
 	responseBody.SchemaItem = responseBodyItem
 	responseBody.Code = "200"
 
+	endpointInterface := model.EndpointInterface{}
 	endpointInterface.BodyType = s.getBodyType(functionDetail.RequestType)
 	endpointInterface.Method = consts.POST
 	endpointInterface.RequestBody = requestBody
@@ -414,9 +396,6 @@ func (s *ThirdPartySyncService) GenerateEndpoint(endpointId uint, functionDetail
 	res.Title = functionDetail.ClassCode + "-" + functionDetail.Code
 	res.Path = "/" + functionDetail.ServiceCode + "/" + functionDetail.ClassCode + "/" + functionDetail.Code
 	res.Interfaces = []model.EndpointInterface{endpointInterface}
-	//res.Interfaces[0].BodyType = s.getBodyType(functionDetail.RequestType)
-	//res.Interfaces[0].RequestBody = requestBody
-	//res.Interfaces[0].ResponseBodies[0] = responseBody
 
 	return
 }
@@ -521,12 +500,11 @@ func (s *ThirdPartySyncService) ImportThirdPartyFunctions(req v1.ImportEndpointD
 			continue
 		}
 
-		newEndpointDetail, err := s.GenerateEndpoint(endpoint.ID, functionDetail)
+		newEndpointDetail, err := s.GenerateEndpoint(functionDetail)
 		if err != nil && err != gorm.ErrRecordNotFound {
 			continue
 		}
 
-		//newEndpointDetail.ServeId = 0
 		newSnapshot := _commUtils.JsonEncode(s.EndpointService.Yaml(newEndpointDetail))
 
 		if req.DataSyncType == consts.AutoAdd && endpoint.ID != 0 {
@@ -571,13 +549,6 @@ func (s *ThirdPartySyncService) ImportThirdPartyFunctions(req v1.ImportEndpointD
 		}
 
 		if endpoint.ID == 0 {
-			//createEndpointDetail, err := s.EndpointRepo.GetAll(endpointId, "v0.1.0")
-			//if err != nil {
-			//	continue
-			//}
-			//
-			//createEndpointDetail.ServeId = 0
-			//snapshot := _commUtils.JsonEncode(s.EndpointService.Yaml(createEndpointDetail))
 			err = s.EndpointRepo.ChangeSnapShot(endpointId, newSnapshot)
 			if err != nil {
 				continue

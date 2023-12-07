81871e927ef835e798661f450f3d783216903571
wangzhen <wangzhen@nancal.com>
2023-12-07 13:46:31 +0800

    feat:#LYAPI-1371 获取当前请求origin放到中间件

--- internal/pkg/core/middleware/error.go
+++ internal/pkg/core/middleware/error.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"fmt"
-	"github.com/aaronchen2k/deeptest/internal/server/core/cache"
 	_domain "github.com/aaronchen2k/deeptest/pkg/domain"
 	logUtils "github.com/aaronchen2k/deeptest/pkg/lib/log"
 	"github.com/kataras/iris/v12"
@@ -13,7 +12,6 @@ import (
 func Error() iris.Handler {
 
 	return func(ctx *context.Context) {
-		cache.SetCache("host", ctx.Request().Header.Get("Origin"), -1)
 		defer func(ctx *context.Context) {
 			if err := recover(); err != nil {
 				logUtils.Info(fmt.Sprintf("%v", err))

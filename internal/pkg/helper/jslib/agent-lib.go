3033cbd978e2a0daf0687b1398ed7a5c5ddcb3d3
wangzhen <wangzhen@nancal.com>
2024-01-17 14:09:19 +0800

    feat:#过滤掉health接口

--- internal/pkg/helper/jslib/agent-lib.go
+++ internal/pkg/helper/jslib/agent-lib.go
@@ -61,7 +61,7 @@ func RefreshRemoteAgentJslibs(runtime *goja.Runtime, require *require.RequireMod
 		id := lib.Id
 
 		updateTime, ok := GetAgentCache(projectId, id)
-
+		logUtils.Infof("更新第三方库，projectId：%v,ok:%v,updateTime:%v,UpdatedAt:%v", projectId, ok, updateTime, lib.UpdatedAt)
 		if !ok || updateTime.Before(lib.UpdatedAt) {
 			pth := filepath.Join(consts.TmpDir, fmt.Sprintf("%d.js", id))
 			fileUtils.WriteFile(pth, lib.Script)
@@ -73,6 +73,7 @@ func RefreshRemoteAgentJslibs(runtime *goja.Runtime, require *require.RequireMod
 			runtime.Set(lib.Name, module)
 
 			SetAgentCache(projectId, id, lib.UpdatedAt)
+			logUtils.Infof("更新第三方库，projectId：%v,id:%v,lib.Name:%v", projectId, id, lib.Name)
 		}
 	}
 }

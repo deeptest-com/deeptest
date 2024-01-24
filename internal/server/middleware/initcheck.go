4926064ba2893397cfc0f85a156e00212daaebc8
zhourui <zhourui@nancal.com>
2024-01-24 16:47:57 +0800

    feat:#LYAPI-1539 ly集成二期 去除日志

--- internal/server/middleware/initcheck.go
+++ internal/server/middleware/initcheck.go
@@ -5,7 +5,6 @@ import (
 	"github.com/aaronchen2k/deeptest/internal/server/core/cache"
 	"github.com/aaronchen2k/deeptest/internal/server/core/dao"
 	"github.com/aaronchen2k/deeptest/pkg/domain"
-	logUtils "github.com/aaronchen2k/deeptest/pkg/lib/log"
 	"net/http"
 
 	"github.com/kataras/iris/v12"
@@ -18,21 +17,14 @@ func InitCheck() iris.Handler {
 		if dao.GetDB() == nil || (config.CONFIG.System.CacheType == "redis" && config.CACHE == nil) {
 			ctx.StopWithJSON(http.StatusOK, _domain.Response{Code: _domain.NeedInitErr.Code, Data: nil, Msg: _domain.NeedInitErr.Msg})
 		} else {
-			logUtils.Infof("initHostParam X-Token:%s,Origin:%s,X-API-Origin:%s", ctx.GetHeader("X-Token"), ctx.Request().Header.Get("Origin"), ctx.Request().Header.Get("X-API-Origin"))
 			host := ctx.Request().Header.Get("Origin")
-			//thirdPartyHost := ctx.Request().Header.Get("Origin")
 
 			if ctx.GetHeader("X-Token") != "" {
 				host = ctx.Request().Header.Get("X-API-Origin")
-				//thirdPartyHost = ctx.Request().Header.Get("Origin")
 			}
 			if host != "" {
 				cache.SetCache("host", host, -1)
 			}
-			//if thirdPartyHost != "" {
-			//	err := cache.SetCache("thirdPartyHost", thirdPartyHost, -1)
-			//	logUtils.Infof("initThirdPartyHost thirdPartyHost:%s, Err:%+v", thirdPartyHost, err)
-			//}
 			ctx.Next()
 		}
 

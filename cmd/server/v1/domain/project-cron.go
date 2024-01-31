827138d8fd219ef909e3f0854664f431facdbf36
wangzhen <wangzhen@nancal.com>
2024-01-31 09:21:42 +0800

    #LYAPI-1192 同步任务

--- cmd/server/v1/domain/project-cron.go
+++ cmd/server/v1/domain/project-cron.go
@@ -3,12 +3,14 @@ package serverDomain
 import "github.com/aaronchen2k/deeptest/internal/pkg/consts"
 
 type SaveProjectCronReq struct {
-	Id         uint              `json:"id"`
-	Name       string            `json:"name"`
-	Cron       string            `json:"cron"`
-	Source     consts.CronSource `json:"source"`
-	SwaggerReq SwaggerCronReq    `json:"swaggerReq"`
-	LecangReq  LecangCronReq     `json:"lecangReq"`
+	Id         uint                `json:"id"`
+	Name       string              `json:"name"`
+	Cron       string              `json:"cron"`
+	ProjectId  uint                `json:"projectId"`
+	Source     consts.CronSource   `json:"source"`
+	Switch     consts.SwitchStatus `json:"switch"`
+	SwaggerReq SwaggerSyncReq      `json:"swaggerReq"`
+	LecangReq  LecangCronReq       `json:"lecangReq"`
 }
 
 type SwaggerCronReq struct {

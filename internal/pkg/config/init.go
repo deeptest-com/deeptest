8226e24ade3dca6a0f280bddd0d4106c939c6076
chenqi99 <chenqi99@nancal.com>
2024-02-21 09:27:57 +0800

    read config

--- internal/pkg/config/init.go
+++ internal/pkg/config/init.go
@@ -12,6 +12,7 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/snowlyg/helper/dir"
 	"github.com/spf13/viper"
+	"os"
 	"path/filepath"
 )
 
@@ -24,16 +25,17 @@ var (
 
 func Init() {
 	consts.IsRelease = _commUtils.IsRelease()
+	getWorkdir()
 
 	v := viper.New()
 	VIPER = v
 	VIPER.SetConfigType("yaml")
 
-	// agent
+	// for agent
 	if consts.RunFrom == consts.FromAgent {
 		agentConfigPath := filepath.Join(consts.WorkDir, consts.AgentConfigFileName)
 
-		if !dir.IsExist(agentConfigPath) { // 没有配置文件，写入默认配置
+		if !dir.IsExist(agentConfigPath) { // create config if not exist
 			configRes := filepath.Join("res", consts.AgentConfigFileName)
 			yamlDefault, _ := deeptest.ReadResData(configRes)
 
@@ -46,10 +48,16 @@ func Init() {
 			}
 		}
 
+		// read config
 		VIPER.SetConfigFile(agentConfigPath)
 		err := VIPER.ReadInConfig()
 		if err != nil {
 			panic(fmt.Errorf("读取配置错误: %w ", err))
+		}
+
+		// load config
+		if err := VIPER.Unmarshal(&CONFIG); err != nil {
+			panic(fmt.Errorf("解析配置文件错误: %w ", err))
 		}
 
 		if consts.Port > 0 {
@@ -61,13 +69,47 @@ func Init() {
 		if CONFIG.System.Name == "" {
 			CONFIG.System.Name = consts.App
 		}
-		getWorkdir(CONFIG.System.Name)
 
 		return
 	}
 
-	// server
-	// 初始化rbac_model.conf配置
+	// server config
+	serverConfigPath := filepath.Join(consts.WorkDir, consts.ServerConfigFileName)
+	if !dir.IsExist(serverConfigPath) { // create config if not exist
+		configRes := filepath.Join("res", consts.ServerConfigFileName)
+		yamlDefault, _ := deeptest.ReadResData(configRes)
+
+		if err := VIPER.ReadConfig(bytes.NewBuffer(yamlDefault)); err != nil {
+			panic(fmt.Errorf("读取默认配置文件错误: %w ", err))
+		}
+
+		if err := VIPER.WriteConfigAs(serverConfigPath); err != nil {
+			panic(fmt.Errorf("写入配置文件错误: %w ", err))
+		}
+	}
+
+	// read config
+	VIPER.SetConfigFile(serverConfigPath)
+	err := VIPER.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("读取配置错误: %w ", err))
+	}
+
+	// watch config
+	VIPER.WatchConfig()
+	VIPER.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("配置发生变化:", e.Name)
+		if err := VIPER.Unmarshal(&CONFIG); err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	// load config
+	if err := VIPER.Unmarshal(&CONFIG); err != nil {
+		fmt.Println(err)
+	}
+
+	// create casbin rbac_model.conf if needed
 	casbinPath := filepath.Join(consts.WorkDir, consts.CasbinFileName)
 	if !dir.IsExist(casbinPath) {
 		casbinRes := filepath.Join("res", consts.CasbinFileName)
@@ -82,50 +124,18 @@ func Init() {
 		}
 	}
 
-	// 初始化server.yaml配置
-	serverConfigPath := filepath.Join(consts.WorkDir, consts.ServerConfigFileName)
-	if !dir.IsExist(serverConfigPath) { // 没有配置文件，写入默认配置
-		configRes := filepath.Join("res", consts.ServerConfigFileName)
-		yamlDefault, _ := deeptest.ReadResData(configRes)
-
-		if err := VIPER.ReadConfig(bytes.NewBuffer(yamlDefault)); err != nil {
-			panic(fmt.Errorf("读取默认配置文件错误: %w ", err))
-		}
-
-		if err := VIPER.WriteConfigAs(serverConfigPath); err != nil {
-			panic(fmt.Errorf("写入配置文件错误: %w ", err))
-		}
-	}
-
-	VIPER.SetConfigFile(serverConfigPath)
-	err := VIPER.ReadInConfig()
-	if err != nil {
-		panic(fmt.Errorf("读取配置错误: %w ", err))
-	}
-
-	// 监控配置文件变化
-	VIPER.WatchConfig()
-	VIPER.OnConfigChange(func(e fsnotify.Event) {
-		fmt.Println("配置发生变化:", e.Name)
-		if err := VIPER.Unmarshal(&CONFIG); err != nil {
-			fmt.Println(err)
-		}
-	})
-
-	if err := VIPER.Unmarshal(&CONFIG); err != nil {
-		fmt.Println(err)
-	}
-
-	getWorkdir(CONFIG.System.Name)
-
 	CONFIG.System.SysEnv = _commUtils.GetEnvVar("SysEnv", CONFIG.System.SysEnv)
 	myZap.ZapInst = CONFIG.Zap
 }
 
-func getWorkdir(appName string) {
-	home, _ := _fileUtils.GetUserHome()
-	consts.WorkDir = filepath.Join(home, appName)
-	consts.TmpDir = filepath.Join(consts.WorkDir, consts.FolderTmp)
+func getWorkdir() {
+	consts.WorkDir = os.Getenv("WORK_DIR")
 
+	if consts.WorkDir == "" {
+		userHome, _ := _fileUtils.GetUserHome()
+		consts.WorkDir = filepath.Join(userHome, consts.App)
+	}
+
+	consts.TmpDir = filepath.Join(consts.WorkDir, consts.FolderTmp)
 	_fileUtils.MkDirIfNeeded(consts.TmpDir)
 }

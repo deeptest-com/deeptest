71476058981ef3c9e3e48f16cbe5b7ba8cfe8bda
wangzhen <wangzhen@nancal.com>
2024-05-20 11:40:54 +0800

    feat:#LYAPI-2145 saas环境获取请求域名

--- saas/common/func.go
+++ saas/common/func.go
@@ -2,26 +2,29 @@ package common
 
 import (
 	"fmt"
+	"github.com/aaronchen2k/deeptest/internal/pkg/config"
 	"github.com/aaronchen2k/deeptest/internal/pkg/consts"
 	logUtils "github.com/aaronchen2k/deeptest/pkg/lib/log"
+	"github.com/aaronchen2k/deeptest/saas/tenant"
 	"github.com/kataras/iris/v12/context"
 	"runtime/debug"
+	"strings"
 )
 
 func GetTenantId(ctx *context.Context) consts.TenantId {
 	ret := ctx.GetHeader("tenantId")
 	//根据域名获取租户
 	tenantId := consts.TenantId(ret)
-	/*
-		if config.CONFIG.Saas.Switch && ret == "" {
-			fmt.Println(ctx.Host(), "++++++++++++++++")
-			domain := ctx.Domain()
-			array := strings.Split(domain, ".")
-			prefix := strings.ReplaceAll(array[0], "dev-", "")
-			info := tenant.NewTenant().GetInfo(tenantId, prefix)
-			tenantId = info.Id
-		}
-	*/
+
+	if config.CONFIG.Saas.Switch && ret == "" {
+		fmt.Println(ctx.Host(), "++++++++++++++++")
+		domain := ctx.Domain()
+		array := strings.Split(domain, ".")
+		prefix := strings.ReplaceAll(array[0], "dev-", "")
+		info := tenant.NewTenant().GetInfo(tenantId, prefix)
+		tenantId = info.Id
+	}
+
 	return tenantId
 }
 

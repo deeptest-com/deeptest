d5a387a7d4fcacc4ca90e4f036f2fe027ae2aa92
wangzhen <wangzhen@nancal.com>
2024-05-20 14:48:23 +0800

    feat:#LYAPI-2145 saas mock bug

--- saas/common/func.go
+++ saas/common/func.go
@@ -5,8 +5,10 @@ import (
 	"github.com/aaronchen2k/deeptest/internal/pkg/config"
 	"github.com/aaronchen2k/deeptest/internal/pkg/consts"
 	logUtils "github.com/aaronchen2k/deeptest/pkg/lib/log"
+	"github.com/aaronchen2k/deeptest/saas/tenant"
 	"github.com/kataras/iris/v12/context"
 	"runtime/debug"
+	"strings"
 )
 
 func GetTenantId(ctx *context.Context) consts.TenantId {
@@ -14,16 +16,13 @@ func GetTenantId(ctx *context.Context) consts.TenantId {
 	//根据域名获取租户
 	tenantId := consts.TenantId(ret)
 
-	if config.CONFIG.Saas.Switch && ret == "" {
-
+	if config.CONFIG.Saas.Switch && ret == "" && strings.HasPrefix(ctx.Path(), "/mocks") {
 		fmt.Println(ctx.Host(), ctx.Path(), "++++++++++++++++")
-		/*
-			domain := ctx.Domain()
-			array := strings.Split(domain, ".")
-			prefix := strings.ReplaceAll(array[0], "dev-", "")
-			info := tenant.NewTenant().GetInfo(tenantId, prefix)
-			tenantId = info.Id
-		*/
+		domain := ctx.Host()
+		array := strings.Split(domain, ".")
+		prefix := strings.ReplaceAll(array[0], "dev-", "")
+		info := tenant.NewTenant().GetInfo(tenantId, prefix)
+		tenantId = info.Id
 	}
 
 	return tenantId

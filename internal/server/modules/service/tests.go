78a937f0f5e58cfb2e3bbaf12f1f693547003d60
chenqi99 <chenqi99@nancal.com>
2024-09-23 09:59:50 +0800

    mock stream response

--- internal/server/modules/service/tests.go
+++ internal/server/modules/service/tests.go
@@ -6,6 +6,7 @@ import (
 	serverDomain "github.com/aaronchen2k/deeptest/cmd/server/v1/domain"
 	"github.com/kataras/iris/v12"
 	"net/http"
+	"time"
 )
 
 type StreamTestService struct {
@@ -13,15 +14,19 @@ type StreamTestService struct {
 
 func (s *StreamTestService) Chat(req serverDomain.StreamTestObj, flusher http.Flusher, ctx iris.Context) (err error) {
 	bts, err := json.Marshal(req)
-	ctx.Writef("%s\n\n", string(bts))
+	ctx.Writef("data: request is %s\n\n", string(bts))
 
 	for i := 0; i < req.Count; i++ {
 		fmt.Printf("\n>>> %d \n", i)
 
-		// must with prefix "data:" for openai response
-		// must add a postfix "\n\n"
-		ctx.Writef("index = %d\n\n", i)
+		bts, _ := json.Marshal(iris.Map{"index": i + 1})
+
+		// must add prefix "data:" for openai response
+		// must add postfix "\n\n"
+		ctx.Writef("data:%s\n\n", string(bts))
 		flusher.Flush()
+
+		time.Sleep(1 * time.Second)
 	}
 
 	return

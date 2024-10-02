8ebecd826050555d3da07d697f9ab76c9292604c
chenqi99 <chenqi99@nancal.com>
2024-10-02 14:11:49 +0800

    ai measurement

--- resData.go
+++ resData.go
@@ -4,16 +4,30 @@ import (
 	"embed"
 	commonUtils "github.com/deeptest-com/deeptest/pkg/lib/comm"
 	"os"
+	"path/filepath"
 )
 
 //go:embed res
 var resFileSys embed.FS
 
-func ReadResData(path string) (ret []byte, err error) {
+func ReadResData(pth string) (ret []byte, err error) {
 	if commonUtils.IsRelease() {
-		ret, err = resFileSys.ReadFile(path)
+		ret, err = resFileSys.ReadFile(pth)
 	} else {
-		ret, err = os.ReadFile(path)
+		ret, err = os.ReadFile(pth)
+	}
+
+	return
+}
+
+//go:embed internal/agent/_prompt_templ
+var promptFileSys embed.FS
+
+func ReadPromptTempl(pth string) (ret []byte, err error) {
+	if commonUtils.IsRelease() {
+		ret, err = promptFileSys.ReadFile(pth)
+	} else {
+		ret, err = os.ReadFile(filepath.Join("internal", "agent", "_prompt_templ", pth))
 	}
 
 	return
